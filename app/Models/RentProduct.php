9460532aad6c826e70cc88d9f989bd94ac95dd07
Fumino The Fish <richberchannel01@gmail.com>
2025-09-07 15:23:14 +0700

    fix: remove unused import in RentProduct model

--- app/Models/RentProduct.php
+++ app/Models/RentProduct.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Testing\Fluent\Concerns\Has;
+
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 
