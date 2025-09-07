22840ba4ed83c2a72590abca28576323af16ded7
Fumino The Fish <richberchannel01@gmail.com>
2025-09-07 14:05:33 +0700

    refactor: correct comment for model relationships in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,8 +50,7 @@ class User extends Authenticatable
         ];
     }
 
-    //model relationship
-
+    //model relationships
     public function partnerProfile()
     {
         return $this->hasOne(PartnerProfile::class);
