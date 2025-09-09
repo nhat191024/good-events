50d576fa08f7c7d4e5de01a0de9e58562672b4b3
Fumino The Fish <richberchannel01@gmail.com>
2025-09-09 13:07:13 +0700

    feat: add English and Vietnamese language support for user fields

---
+++ lang/en/admin/user.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'user' => 'Users',
+
+    'fields' => [
+        'label' => [
+            'name' => 'Name',
+            'avatar' => 'Avatar',
+            'email' => 'Email address',
+            'country_code' => 'Country code',
+            'phone' => 'Phone number',
+            'password' => 'Password',
+            'email_verified_at' => 'Email verified at',
+        ],
+
+        'placeholder' => [
+            'name' => 'Enter name',
+            'email' => 'Enter email address',
+            'country_code' => 'Enter country code',
+            'phone' => 'Enter phone number',
+            'password' => 'Enter password',
+        ],
+    ]
+];
---
+++ lang/vi/admin/user.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'user' => 'Người dùng',
+
+    'fields' => [
+        'label' => [
+            'name' => 'Tên',
+            'avatar' => 'Ảnh đại diện',
+            'email' => 'Địa chỉ email',
+            'country_code' => 'Mã quốc gia',
+            'phone' => 'Số điện thoại',
+            'password' => 'Mật khẩu',
+            'email_verified_at' => 'Email đã xác minh',
+        ],
+
+        'placeholder' => [
+            'name' => 'Nhập tên',
+            'email' => 'Nhập địa chỉ email',
+            'country_code' => 'Nhập mã quốc gia',
+            'phone' => 'Nhập số điện thoại',
+            'password' => 'Nhập mật khẩu',
+        ],
+    ]
+];
