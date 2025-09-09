4efc5dde53e30967dce79921104a42e53868eac8
Fumino The Fish <richberchannel01@gmail.com>
2025-09-09 13:07:09 +0700

    feat: add English and Vietnamese language support for admin action labels

---
+++ lang/en/admin/action.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'delete_at' => 'Delete at',
+    'created_at' => 'Created at',
+    'updated_at' => 'Updated at',
+];
---
+++ lang/vi/admin/action.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'delete_at' => 'Xóa lúc',
+    'created_at' => 'Tạo lúc',
+    'updated_at' => 'Cập nhật lúc',
+];
