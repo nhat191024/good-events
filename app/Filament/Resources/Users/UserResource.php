c7935aeb586194aa88720ed8d262f97730c2be58
Fumino The Fish <richberchannel01@gmail.com>
2025-09-09 16:37:06 +0700

    fix: update getEloquentQuery to use Role::CLIENT for user role filtering

--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -20,6 +20,8 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
+use App\Enum\Role;
+
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
@@ -44,7 +46,7 @@ class UserResource extends Resource
     public static function getEloquentQuery(): Builder
     {
         return parent::getEloquentQuery()
-            ->role('client');
+            ->role(Role::CLIENT);
     }
 
     public static function getRelations(): array
