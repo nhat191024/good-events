bb7fb388bd7cfed883af7ad30c13dff863aaa965
Fumino The Fish <richberchannel01@gmail.com>
2025-09-09 16:19:22 +0700

    feat: add PartnerSeeder for seeding partner users

---
+++ database/seeders/PartnerSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Enum\Role;
+
+class PartnerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()
+            ->withRole(Role::PARTNER)
+            ->count(10)
+            ->createPartner()
+            ->create();
+    }
+}
