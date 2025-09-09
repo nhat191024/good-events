9bae4aede0b9dad4267bfb756d9b38b7fca111ae
Fumino The Fish <richberchannel01@gmail.com>
2025-09-09 19:36:31 +0700

    feat: add Vietnamese language support for partner fields

---
+++ lang/en/admin/partner.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    'partner' => 'Partner',
+
+    'fields' => [
+        'label' => [
+            'name' => 'Name',
+            'partner_name' => 'Partner Nickname',
+            'avatar' => 'Avatar',
+            'email' => 'Email Address',
+            'country_code' => 'Country Code',
+            'phone' => 'Phone Number',
+            'identity_card_number' => 'ID Card Number',
+            'password' => 'Password',
+            'email_verified_at' => 'Email Verified At',
+        ],
+
+        'placeholder' => [
+            'name' => 'Enter name',
+            'partner_name' => 'Enter partner nickname',
+            'email' => 'Enter email address',
+            'country_code' => 'Enter country code',
+            'phone' => 'Enter phone number',
+            'password' => 'Enter password',
+        ],
+    ]
+];
---
+++ lang/vi/admin/partner.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    'partner' => 'Đối tác',
+
+    'fields' => [
+        'label' => [
+            'name' => 'Tên',
+            'partner_name' => 'Biệt danh đối tác',
+            'avatar' => 'Ảnh đại diện',
+            'email' => 'Địa chỉ email',
+            'country_code' => 'Mã quốc gia',
+            'phone' => 'Số điện thoại',
+            'identity_card_number' => 'Số CMND/CCCD',
+            'password' => 'Mật khẩu',
+            'email_verified_at' => 'Email đã xác minh',
+        ],
+
+        'placeholder' => [
+            'name' => 'Nhập tên',
+            'partner_name' => 'Nhập biệt danh đối tác',
+            'email' => 'Nhập địa chỉ email',
+            'country_code' => 'Nhập mã quốc gia',
+            'phone' => 'Nhập số điện thoại',
+            'password' => 'Nhập mật khẩu',
+        ],
+    ]
+];
