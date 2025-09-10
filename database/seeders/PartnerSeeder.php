f6660e48eb13f5a3360ffa9f61cd7814215ad4f8
Fumino The Fish <richberchannel01@gmail.com>
2025-09-10 20:53:20 +0700

    feat: add single partner user creation to PartnerSeeder

--- database/seeders/PartnerSeeder.php
+++ database/seeders/PartnerSeeder.php
@@ -16,6 +16,14 @@ class PartnerSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->withRole(Role::PARTNER)
+            ->count(1)
+            ->createPartner()
+            ->create([
+                'name' => 'Partner User',
+                'email' => 'partner@example.com',
+            ]);
+
         User::factory()
             ->withRole(Role::PARTNER)
             ->count(10)
