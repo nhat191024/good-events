4e7da89d24f6d36a1227cb8517e03c84b1f5d677
Fumino The Fish <richberchannel01@gmail.com>
2025-09-15 16:03:54 +0700

    feat: add BILLING case to FilamentNavigationGroup enum with corresponding label

--- app/Enum/FilamentNavigationGroup.php
+++ app/Enum/FilamentNavigationGroup.php
@@ -7,11 +7,13 @@ use Filament\Support\Contracts\HasLabel;
 enum FilamentNavigationGroup implements HasLabel
 {
     case USER_MANAGEMENT;
+    case BILLING;
 
     public function getLabel(): string
     {
         return match ($this) {
             self::USER_MANAGEMENT => __('admin\sidebar.user_management'),
+            self::BILLING => __('admin\sidebar.billing'),
         };
     }
 }
