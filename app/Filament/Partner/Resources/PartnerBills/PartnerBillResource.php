1357430f426345121145ca684373c40c72021c61
Fumino The Fish <richberchannel01@gmail.com>
2025-09-15 16:09:02 +0700

    feat: implement PartnerBill resource with list pages

---
+++ app/Filament/Partner/Resources/PartnerBills/PartnerBillResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Partner\Resources\PartnerBills;
+
+use App\Models\PartnerBill;
+
+use BackedEnum;
+use UnitEnum;
+
+use App\Filament\Partner\Resources\PartnerBills\Pages\CreatePartnerBill;
+use App\Filament\Partner\Resources\PartnerBills\Pages\EditPartnerBill;
+use App\Filament\Partner\Resources\PartnerBills\Pages\ListPartnerBills;
+use App\Filament\Partner\Resources\PartnerBills\Schemas\PartnerBillForm;
+use App\Filament\Partner\Resources\PartnerBills\Tables\PartnerBillsTable;
+
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+
+use Illuminate\Database\Eloquent\Builder;
+
+use App\Enum\FilamentNavigationGroup;
+
+class PartnerBillResource extends Resource
+{
+    protected static ?string $model = PartnerBill::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::CurrencyDollar;
+    protected static string|UnitEnum|null $navigationGroup = FilamentNavigationGroup::BILLING;
+
+    public static function getModelLabel(): string
+    {
+        return __('partner\bill.bill');
+    }
+
+    public static function form(Schema $schema): Schema
+    {
+        return PartnerBillForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return PartnerBillsTable::configure($table);
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->where('partner_id', auth()->id())
+            ->with(['client', 'category', 'event']);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListPartnerBills::route('/'),
+            'create' => CreatePartnerBill::route('/create'),
+            // 'edit' => EditPartnerBill::route('/{record}/edit'),
+        ];
+    }
+}
