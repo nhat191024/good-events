d9285c87a21e3cf74559be0ea7ef6f56527999d6
Fumino The Fish <richberchannel01@gmail.com>
2025-09-18 11:13:47 +0700

    feat: add Vietnamese language support for partner service

---
+++ lang/vi/partner/service.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    'service' => 'Dịch vụ',
+    'your_services' => 'Dịch vụ của bạn',
+    'no_video' => 'Chưa có video',
+    'no_video_description' => 'Dịch vụ này chưa có video giới thiệu nào.',
+
+    'label' => [
+        'category' => 'Danh mục',
+        'status' => 'Trạng thái duyệt',
+        'deleted_at' => 'Ngày xóa',
+        'created_at' => 'Ngày tạo',
+        'updated_at' => 'Ngày cập nhật',
+    ],
+
+    'button' => [
+        'add_service' => 'Thêm dịch vụ',
+        'watch_videos' => 'Xem video'
+    ],
+
+    'status_pending' => 'Chờ duyệt',
+    'status_approved' => 'Đã duyệt',
+    'status_rejected' => 'Từ chối',
+];
