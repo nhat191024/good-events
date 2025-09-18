de34e9fc7f5a75d1413dc92bcdace3c8f93a2a81
Fumino The Fish <richberchannel01@gmail.com>
2025-09-18 16:38:22 +0700

    feat: add Vietnamese language support for profile resource

---
+++ lang/en/profile.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'profile' => 'Profile',
+    'user_info' => 'User Information',
+    'user_info_description' => 'Update your basic information.',
+    'label' => [
+        'avatar' => 'Avatar',
+        'name' => 'Full Name',
+        'email' => 'Email',
+        'country_code' => 'Country Code',
+        'phone' => 'Phone Number',
+    ],
+
+    'partner_info' => 'Partner Information',
+    'partner_info_description' => 'Update your partner profile information.',
+    'partner_label' => [
+        'partner_name' => 'Partner/Business Name',
+        'identity_card_number' => 'Identity Card Number',
+        'location_id' => 'Location',
+    ],
+
+    'buttons' => [
+        'save_changes' => 'Save Changes',
+    ],
+];
