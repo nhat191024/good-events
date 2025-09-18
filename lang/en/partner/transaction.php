09c0d552befaf86e3aa3729b466bb7a494405edf
Fumino The Fish <richberchannel01@gmail.com>
2025-09-18 13:00:44 +0700

    feat: add Vietnamese language support for transaction resource

---
+++ lang/en/partner/transaction.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    'wallet' => 'Wallet',
+    'no_transaction' => 'No transactions',
+    'no_transaction_description' => 'You have no transactions in your wallet yet.',
+
+    'types' => [
+        'deposit' => 'Deposit',
+        'withdrawal' => 'Withdrawal',
+    ],
+
+    'label' => [
+        'id' => 'ID',
+        'type' => 'Transaction Type',
+        'amount' => 'Amount',
+        'reason' => 'Reason',
+        'balance' => 'Current Balance',
+        'created_at' => 'Created At',
+    ],
+
+    'button' => [
+        'add_funds' => 'Add Funds',
+    ],
+];
---
+++ lang/vi/partner/transaction.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    'wallet' => 'Ví tiền',
+    'no_transaction' => 'Chưa có giao dịch',
+    'no_transaction_description' => 'Bạn chưa có giao dịch nào trong ví.',
+
+    'types' => [
+        'deposit' => 'Nạp tiền',
+        'withdrawal' => 'Rút tiền',
+    ],
+
+    'label' => [
+        'id' => 'ID',
+        'type' => 'Loại giao dịch',
+        'amount' => 'Số tiền',
+        'reason' => 'Lý do',
+        'balance' => 'Số dư hiện tại',
+        'created_at' => 'Thời gian',
+    ],
+
+    'button' => [
+        'add_funds' => 'Nạp tiền',
+    ],
+];
