180a3d5f05ccf0a6ef1d8756125ce775c776ce7b
Fumino The Fish <richberchannel01@gmail.com>
2025-09-20 03:42:34 +0700

    fix: make min_price and max_price nullable in partner_categories migration

--- database/migrations/2025_09_05_074458_create_partner_categories_table.php
+++ database/migrations/2025_09_05_074458_create_partner_categories_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('slug')->unique();
             $table->foreignId('parent_id')->nullable()->constrained('partner_categories')->nullOnDelete();
-            $table->double('min_price');
-            $table->double('max_price');
+            $table->double('min_price')->nullable();
+            $table->double('max_price')->nullable();
             $table->text('description')->nullable();
             $table->softDeletes();
             $table->timestamps();
