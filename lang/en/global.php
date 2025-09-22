b1d3b22faee434f8dd34e1f62a8a004ca5ed1fb0
Fumino The Fish <richberchannel01@gmail.com>
2025-09-22 13:18:40 +0700

    feat: add balance and currency translations to global language files

---
+++ lang/en/global.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'hidden' => 'Hide',
+    'show' => 'Show',
+    'edit' => 'Edit',
+
+    'balance' => 'Balance',
+    'currency' => 'VND',
+];
--- lang/vi/global.php
+++ lang/vi/global.php
@@ -3,4 +3,7 @@ return [
     'hidden' => 'Ẩn',
     'show' => 'Hiện',
     'edit' => 'Sửa',
+
+    'balance' => 'Số dư',
+    'currency' => 'VNĐ',
 ];
