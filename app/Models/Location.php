eea0422e8137c95639f6af409874e907ede97702
Fumino The Fish <richberchannel01@gmail.com>
2025-09-25 14:46:41 +0700

    feat: extend Location model with additional properties and query methods

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -7,14 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @property int $id
  * @property string $name
+ * @property string $code
+ * @property string $codename
+ * @property string|null $short_codename
+ * @property string $type
+ * @property string|null $phone_code
+ * @property int|null $parent_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Location newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Location newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Location query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereCodename($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereParentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location wherePhoneCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereShortCodename($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereType($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereUpdatedAt($value)
  * @mixin \Eloquent
  */
