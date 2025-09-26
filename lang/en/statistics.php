cd3ff7ba5bab1f2bb32c5fece76aa27ae1527ea7
Fumino The Fish <richberchannel01@gmail.com>
2025-09-26 23:48:00 +0700

    feat: add English and Vietnamese language files for customer statistics

---
+++ lang/en/statistics.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'number_of_customers' => 'Number of Customers',
+    'total_customers_served' => 'Total customers served',
+    'satisfaction_rate' => 'Satisfaction Rate',
+    'average_customer_rating' => 'Average customer rating',
+    'revenue' => 'Revenue',
+    'total_revenue_generated' => 'Total revenue generated',
+    'orders_placed' => 'Orders Placed',
+    'total_orders_received' => 'Total orders received',
+    'completed_orders' => 'Completed Orders',
+    'number_of_completed_orders' => 'Number of completed orders',
+    'cancellation_rate' => 'Cancellation Rate',
+    'percentage_cancelled_orders' => 'Percentage of cancelled orders',
+];
---
+++ lang/vi/statistics.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'number_of_customers' => 'Số khách hàng',
+    'total_customers_served' => 'Tổng số khách hàng đã phục vụ',
+    'satisfaction_rate' => 'Tỷ lệ hài lòng',
+    'average_customer_rating' => 'Đánh giá trung bình từ khách hàng',
+    'revenue' => 'Doanh thu',
+    'total_revenue_generated' => 'Tổng doanh thu đã tạo ra',
+    'orders_placed' => 'Đơn hàng đã đặt',
+    'total_orders_received' => 'Tổng số đơn hàng đã nhận',
+    'completed_orders' => 'Đơn hàng hoàn thành',
+    'number_of_completed_orders' => 'Số đơn hàng đã hoàn thành',
+    'cancellation_rate' => 'Tỷ lệ hủy đơn',
+    'percentage_cancelled_orders' => 'Phần trăm đơn hàng bị hủy',
+];
