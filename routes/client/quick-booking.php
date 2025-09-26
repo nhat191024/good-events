a95d24d1af928ccea9615368fa9430fa0ab00937
Peredar <64675226+perad4r@users.noreply.github.com>
2025-09-26 21:16:29 +0700

    feat: add quick booking routes for category and partner selection

---
+++ routes/client/quick-booking.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\Client\QuickBookingController;
+use Illuminate\Support\Facades\Route;
+
+//! commented out for development only, users have to be logged in to access these routes
+// Route::middleware('auth')->group(function () {
+    Route::prefix("dat-show")->name('quick-booking.')->group(
+        function () {
+
+            Route::get(
+                '/',
+                [QuickBookingController::class, 'chooseCategory']
+            )->name('choose-category');
+
+            Route::get(
+                '/{partner_category_slug}/chon-loai-doi-tac',
+                [QuickBookingController::class, 'choosePartnerCategory']
+            )->name('choose-partner-category');
+
+            //? we split into 3 separated routes so every step
+            //? can be linked from a detail page (ex: 'BOOK NOW!' button link/href)
+            Route::get(
+                '/{partner_category_slug}/{partner_child_category_slug}/dien-thong-tin',
+                [QuickBookingController::class, 'fillOrderInfo']
+            )->name('fill-info');
+
+            Route::post(
+                '/hoan-thanh',
+                [QuickBookingController::class, 'saveBookingInfo']
+            )->name('save-info');
+        }
+    );
+// });
