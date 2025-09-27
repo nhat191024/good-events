57e75427bb83f0c4bbec70cfc0703e968b560308
Peredar <64675226+perad4r@users.noreply.github.com>
2025-09-27 23:11:07 +0700

    feat: enhance AppServiceProvider to enforce HTTPS and configure Vite for local development

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-
+use Inertia\Inertia;
+use Vite;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Inertia::share([
+            'flash' => function () {
+                return [
+                    'success' => session('success'),
+                    'error'   => session('error'),
+                ];
+            },
+        ]);
     }
 }
