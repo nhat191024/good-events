4ffe23b5b2e0285f624cc9401bde4d5fa847cbb0
Fumino The Fish <richberchannel01@gmail.com>
2025-09-27 15:08:18 +0700

    feat: add English and Vietnamese notification templates for partner show reminders

---
+++ lang/en/notification.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'partner_show_reminder_title' => 'Reminder: Partner show :code is starting soon',
+    'partner_show_reminder_body' => 'The show with code :code is starting on :start_date. Please ensure timely attendance.',
+];
