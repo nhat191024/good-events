2cec5143a27f44ba18102529bba235ef849b8fd9
Fumino The Fish <richberchannel01@gmail.com>
2025-09-30 16:21:38 +0700

    feat: add PartnerManager and PartnerSettings for partner configuration

---
+++ app/Filament/Admin/Pages/PartnerManager.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filament\Admin\Pages;
+
+use UnitEnum;
+use BackedEnum;
+
+use App\Enum\FilamentNavigationGroup;
+
+use App\Settings\PartnerSettings;
+
+use Filament\Pages\SettingsPage;
+use Filament\Support\Icons\Heroicon;
+
+use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Section;
+
+use Filament\Forms\Components\TextInput;
+
+class PartnerManager extends SettingsPage
+{
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedCog6Tooth;
+    protected static string|UnitEnum|null $navigationGroup = FilamentNavigationGroup::SETTINGS;
+
+    protected static string $settings = PartnerSettings::class;
+
+    public static function getNavigationLabel(): string
+    {
+        return __('admin/setting.partner');
+    }
+
+    public function getTitle(): string
+    {
+        return __('admin/setting.partner');
+    }
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Section::make(__('admin/setting.partner_settings'))
+                    ->columnSpanFull()
+                    ->schema([
+                        TextInput::make('minimum_balance')
+                            ->label(__(__('admin/setting.fields.minimum_balance')))
+                            ->required(),
+
+                        TextInput::make('fee_percentage')
+                            ->label(__(__('admin/setting.fields.fee_percentage')))
+                            ->required(),
+                    ]),
+            ]);
+    }
+}
