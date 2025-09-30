58168afe706ade0f23cc25abc47a20e8f3fec0af
Fumino The Fish <richberchannel01@gmail.com>
2025-09-30 16:21:18 +0700

    feat: add English and Vietnamese language settings files

---
+++ lang/vi/admin/setting.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'app' => 'Hệ thống',
+    'app_settings' => 'Các cài đặt liên quan đến hệ thống',
+    'partner' => 'Đối tác',
+    'partner_settings' => 'Các cài đặt liên quan đến đối tác',
+
+    'fields' => [
+        'name' => 'Tên ứng dụng',
+        'logo' => 'Logo ứng dụng',
+        'favicon' => 'Favicon',
+
+        'minimum_balance' => 'Số dư tối thiểu',
+        'fee_percentage' => 'Phần trăm phí',
+    ]
+];
