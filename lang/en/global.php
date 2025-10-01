e22a7b610c6d95a18f9d9e7df0da63ddedafb3ff
Fumino The Fish <richberchannel01@gmail.com>
2025-10-01 21:53:45 +0700

    feat: add additional language entries for restore, delete, create, update, cancel, save, confirm in global language files

--- lang/en/global.php
+++ lang/en/global.php
@@ -3,7 +3,21 @@ return [
     'hidden' => 'Hide',
     'show' => 'Show',
     'edit' => 'Edit',
+    'restore' => 'Restore',
+    'delete' => 'Delete',
+    'create' => 'Create',
+    'update' => 'Update',
+    'cancel' => 'Cancel',
+    'save' => 'Save',
+    'confirm' => 'Confirm',
 
     'balance' => 'Balance',
     'currency' => 'VND',
+
+    'hidden_records' => 'Hidden Records',
+
+    'filter' => [
+        'hidden_include' => 'Include hidden records',
+        'hidden_only' => 'Only hidden records'
+    ]
 ];
--- lang/vi/global.php
+++ lang/vi/global.php
@@ -3,7 +3,28 @@ return [
     'hidden' => 'Ẩn',
     'show' => 'Hiện',
     'edit' => 'Sửa',
+    'restore' => 'Khôi phục',
+    'delete' => 'Xóa',
+    'create' => 'Tạo mới',
+    'update' => 'Cập nhật',
+    'cancel' => 'Hủy',
+    'save' => 'Lưu',
+    'confirm' => 'Xác nhận',
+
+    'no' => 'Không',
+    'yes' => 'Có',
+
+    'list' => 'Danh sách',
+
+    'exists' => 'Tồn tại',
 
     'balance' => 'Số dư',
     'currency' => 'VNĐ',
+
+    'hidden_records' => 'Bản ghi bị ẩn',
+
+    'filter' => [
+        'hidden_include' => 'Bao gồm bản ghi bị ẩn',
+        'hidden_only' => 'Chỉ bản ghi bị ẩn'
+    ]
 ];
