34598538ed6587e6f6633d850555bf42db7f72be
Fumino The Fish <richberchannel01@gmail.com>
2025-10-01 21:53:50 +0700

    feat: add Vietnamese language entries for partner categories in admin panel

---
+++ lang/vi/admin/partnerCategory.php
@@ -0,0 +1,41 @@
+<?php
+return [
+    'singular' => 'Sự kiện',
+    'plural' => 'Danh mục sự kiện',
+
+    'manage_event_categories' => 'Quản lý danh mục :name',
+
+    'partner_categories' => 'Danh mục đối tác',
+    'manage_partner_categories' => 'Quản lý danh mục đối tác',
+
+    'edit_partner_category' => 'Chỉnh sửa danh mục đối tác',
+    'create_partner_category' => 'Tạo danh mục đối tác',
+    'edit_partner_category_with_name' => 'Chỉnh sửa :name',
+
+    'cannot_hidden_partner_category_has_services' => 'Không thể ẩn danh mục có có dịch vụ đối tác',
+    'cannot_hidden_category_has_services' => 'Không thể ẩn danh mục có có dịch vụ',
+
+    'fields' => [
+        'name' => 'Tên',
+        'slug' => 'Slug',
+        'min_price' => 'Giá thấp nhất',
+        'max_price' => 'Giá cao nhất',
+
+        'description' => 'Mô tả',
+
+        'partner_category' => 'Danh mục đối tác',
+
+        'created_at' => 'Ngày tạo',
+        'updated_at' => 'Ngày cập nhật',
+        'deleted_at' => 'Ngày xóa',
+    ],
+
+    'placeholders' => [
+        'slug' => 'Tự động tạo từ tên',
+    ],
+
+    'actions' => [
+        'create' => 'Thêm danh mục sự kiện',
+        'create_partner_category' => 'Thêm danh mục đối tác',
+    ],
+];
