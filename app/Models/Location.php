ff72f6f8a573f08093f9108cdb8ff174656d2db7
Fumino The Fish <richberchannel01@gmail.com>
2025-10-02 10:14:23 +0700

    feat: add SoftDeletes trait to Location model and migration

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @property int $id
@@ -32,6 +33,8 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Location extends Model
 {
+    use SoftDeletes;
+
     /**
      * The attributes that are mass assignable.
      *
