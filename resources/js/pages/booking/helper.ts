21c96e03e07d08d5ad0b58be7fa10610d88791a5
Peredar <64675226+perad4r@users.noreply.github.com>
2025-10-07 06:56:40 +0700

    feat: fix media source

--- resources/js/pages/booking/helper.ts
+++ resources/js/pages/booking/helper.ts
@@ -1,11 +1,8 @@
-type HasMediaUrl = { original_url?: string, url?: string, preview_url?: string }
-
-/** Get img from the first media object in the list[]
- ** How to use in components: getFirstImg(props.category?.media)
- * @param media Media[]
+/** Get img or fallback
+ * @param media string
  * @returns
  */
-export function getFirstImg(media: HasMediaUrl[] | undefined): string {
+export function getImg(media: string | undefined | null): string {
     const placeHolder = 'https://ui-avatars.com/api/?name=N+A&background=random&size=16';
-    return media?.[0]?.preview_url || media?.[0]?.url || media?.[0]?.original_url || placeHolder;
+    return media || placeHolder;
 }
