e1ecb8d1a9210bbe96232e41590326892b7d6fee
perad4r <64675226+perad4r@users.noreply.github.com>
2025-10-14 20:40:17 +0700

    add: translate verify form to vietnamese

--- resources/js/pages/auth/VerifyEmail.vue
+++ resources/js/pages/auth/VerifyEmail.vue
@@ -1,32 +1,33 @@
 <script setup lang="ts">
-    import TextLink from '@/components/TextLink.vue';
-    import { Button } from '@/components/ui/button';
-    import AuthLayout from '@/layouts/AuthLayout.vue';
-    import { Form, Head } from '@inertiajs/vue3';
-    import { LoaderCircle } from 'lucide-vue-next';
+import TextLink from '@/components/TextLink.vue'
+import { Button } from '@/components/ui/button'
+import AuthLayout from '@/layouts/AuthLayout.vue'
+import { Form, Head } from '@inertiajs/vue3'
+import { LoaderCircle } from 'lucide-vue-next'
 
-    defineProps<{
-        status?: string;
-    }>();
+defineProps<{
+    status?: string
+}>()
 </script>
 
 <template>
-    <AuthLayout title="Verify email"
-        description="Please verify your email address by clicking on the link we just emailed to you.">
+    <AuthLayout title="Xác thực Email"
+        description="Vui lòng xác thực địa chỉ email của bạn bằng cách nhấn vào liên kết mà chúng tôi đã gửi cho bạn.">
 
-        <Head title="Email verification" />
+        <Head title="Xác thực Email" />
 
         <div v-if="status === 'verification-link-sent'" class="mb-4 text-center text-sm font-medium text-green-600">
-            A new verification link has been sent to the email address you provided during registration.
+            Một liên kết xác thực mới đã được gửi đến địa chỉ email bạn đã đăng ký.
         </div>
 
         <Form method="post" :action="route('verification.send')" class="space-y-6 text-center" v-slot="{ processing }">
-            <Button :disabled="processing" variant="secondary">
+            <Button :disabled="processing" variant="secondary" class="text-white font-bold">
                 <LoaderCircle v-if="processing" class="h-4 w-4 animate-spin" />
-                Resend verification email
+                Gửi lại email xác thực
             </Button>
 
-            <TextLink :href="route('logout')" method="post" as="button" class="mx-auto block text-sm"> Log out
+            <TextLink :href="route('logout')" method="post" as="button" class="mx-auto block text-sm">
+                Đăng xuất
             </TextLink>
         </Form>
     </AuthLayout>
