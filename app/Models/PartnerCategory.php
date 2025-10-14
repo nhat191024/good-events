53193b793365fc08a82e1cf21a9fe528b9699026
Fumino The Fish <richberchannel01@gmail.com>
2025-10-14 20:29:34 +0700

    fix: add summary documentation for registerMediaConversions method

--- app/Models/PartnerCategory.php
+++ app/Models/PartnerCategory.php
@@ -83,13 +83,17 @@ class PartnerCategory extends Model implements HasMedia
             ->saveSlugsTo('slug');
     }
 
+    /**
+     * Summary of registerMediaConversions
+     * @param Media|null $media
+     * @return void
+     */
     public function registerMediaConversions(?Media $media = null): void
     {
         $this->addMediaConversion('thumb')
             ->width(300)
             ->height(300)
-            ->sharpen(10)
-            ->nonQueued();
+            ->sharpen(10);
 
         $this->addMediaConversion('mobile_optimized')
             ->width(320)
