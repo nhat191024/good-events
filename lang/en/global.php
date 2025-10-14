5fbab5010f47948f9f00686e028be183c5e6cb59
Fumino The Fish <richberchannel01@gmail.com>
2025-10-14 17:20:06 +0700

    update: add pagination labels and navigation text in Vietnamese language files

--- lang/en/global.php
+++ lang/en/global.php
@@ -11,12 +11,28 @@ return [
     'save' => 'Save',
     'confirm' => 'Confirm',
 
+    'no' => 'No',
+    'yes' => 'Yes',
+
+    'list' => 'List',
+
     'balance' => 'Balance',
     'currency' => 'VND',
 
     'hidden_records' => 'Hidden Records',
 
     'processing' => 'Processing...',
+
+    'next' => 'Next',
+    'previous' => 'Previous',
+
+    'paginate' => [
+        'showing' => 'Showing',
+        'to' => 'to',
+        'of' => 'of',
+        'results' => 'results',
+        'go_to_page' => 'Go to page :page',
+    ],
 
     'filter' => [
         'hidden_include' => 'Include hidden records',
--- lang/vi/global.php
+++ lang/vi/global.php
@@ -25,6 +25,17 @@ return [
 
     'processing' => 'Đang xử lý...',
 
+    'next' => 'Tiếp',
+    'previous' => 'Trước',
+
+    'paginate' => [
+        'showing' => 'Hiển thị',
+        'to' => 'đến',
+        'of' => 'của',
+        'results' => 'kết quả',
+        'go_to_page' => 'Đi đến trang :page',
+    ],
+
     'filter' => [
         'hidden_include' => 'Bao gồm bản ghi bị ẩn',
         'hidden_only' => 'Chỉ bản ghi bị ẩn'
