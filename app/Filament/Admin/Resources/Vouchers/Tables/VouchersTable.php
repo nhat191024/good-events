e55f74b0e614966e67b7785d35d975978617448c
Fumino The Fish <richberchannel01@gmail.com>
2025-10-16 17:13:23 +0700

    refactor: remove searchable option from multiple columns in VouchersTable

--- app/Filament/Admin/Resources/Vouchers/Tables/VouchersTable.php
+++ app/Filament/Admin/Resources/Vouchers/Tables/VouchersTable.php
@@ -31,46 +31,37 @@ class VouchersTable
                         'App\Models\PartnerProfile' => __('admin/voucher.fields.select.model_type.partner'),
                         default => $state,
                     })
-                    ->sortable()
-                    ->searchable(),
+                    ->sortable(),
                 TextColumn::make('data.discount_percent')
                     ->label(__('admin/voucher.fields.label.discount_percent'))
                     ->formatStateUsing(fn($state) => $state . '%')
-                    ->sortable()
-                    ->searchable(),
+                    ->sortable(),
                 TextColumn::make('data.max_discount_amount')
                     ->label(__('admin/voucher.fields.label.max_discount_amount'))
                     ->formatStateUsing(fn($state) => '₫' . number_format($state, 0, ',', '.'))
-                    ->sortable()
-                    ->searchable(),
+                    ->sortable(),
                 TextColumn::make('data.min_order_amount')
                     ->label(__('admin/voucher.fields.label.min_order_amount'))
                     ->formatStateUsing(fn($state) =>  '₫' . number_format($state, 0, ',', '.'))
-                    ->sortable()
-                    ->searchable(),
+                    ->sortable(),
                 TextColumn::make('data.usage_limit')
                     ->label(__('admin/voucher.fields.label.usage_limit'))
-                    ->sortable()
-                    ->searchable(),
+                    ->sortable(),
                 TextColumn::make('data.times_used')
                     ->label(__('admin/voucher.fields.label.times_used'))
-                    ->sortable()
-                    ->searchable(),
+                    ->sortable(),
                 TextColumn::make('data.is_unlimited')
                     ->label(__('admin/voucher.fields.label.is_unlimited'))
                     ->formatStateUsing(fn($state) => $state ? __('global.yes') : __('global.no'))
-                    ->sortable()
-                    ->searchable(),
+                    ->sortable(),
                 TextColumn::make('data.starts_at')
                     ->label(__('admin/voucher.fields.label.starts_at'))
                     ->dateTime()
-                    ->sortable()
-                    ->searchable(),
+                    ->sortable(),
                 TextColumn::make('expires_at')
                     ->label(__('admin/voucher.fields.label.expires_at'))
                     ->dateTime()
-                    ->sortable()
-                    ->searchable(),
+                    ->sortable(),
                 TextColumn::make('created_at')
                     ->label(__('admin/voucher.fields.label.created_at'))
                     ->dateTime()
