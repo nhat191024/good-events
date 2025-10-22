236ceb2e4f40d88f50e355c58d1e908436a36999
Fumino The Fish <richberchannel01@gmail.com>
2025-10-22 20:23:00 +0700

    feat: implement Tag management functionality with Create, Edit, List pages and Tag form

---
+++ app/Filament/Admin/Resources/Tags/Tables/TagsTable.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Filament\Admin\Resources\Tags\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class TagsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label(__('admin/tag.name'))
+                    ->searchable(),
+                TextColumn::make('slug')
+                    ->label(__('admin/tag.slug')),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
