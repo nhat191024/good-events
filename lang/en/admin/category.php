eb9309ec73cb1ffe2ad3ae2a7ad17c2677b9fa47
Fumino The Fish <richberchannel01@gmail.com>
2025-10-23 01:31:00 +0700

    feat: add English and Vietnamese language files for category management

---
+++ lang/en/admin/category.php
@@ -0,0 +1,32 @@
+<?php
+return [
+    'plural' => 'Categories',
+    'singular' => 'Category',
+
+    'cannot_hidden_category_has_children' => 'Cannot hide category because it has child categories.',
+    'manage_children_categories' => 'Manage child categories of :name',
+    'children_category' => 'Child Category',
+
+    'fields' => [
+        'name' => 'Category Name',
+        'slug' => 'Slug',
+        'image' => 'Image',
+        'parents' => 'Parent Categories',
+        'children' => 'Child Categories',
+        'description' => 'Description',
+        'created_at' => 'Created At',
+        'updated_at' => 'Updated At',
+        'deleted_at' => 'Deleted At',
+    ],
+
+    'placeholders' => [
+        'name' => 'Enter category name',
+        'slug' => 'Slug is automatically generated from category name',
+        'description' => 'Enter category description',
+    ],
+
+    'actions' => [
+        'create_child_category' => 'Create Child Category',
+        'manage_children_categories' => 'Manage Child Categories',
+    ]
+];
