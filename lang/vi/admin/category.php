29d79e44e5c911e8bd472c4dcc5c12490a29d988
Fumino The Fish <richberchannel01@gmail.com>
2025-10-23 01:31:00 +0700

    feat: add English and Vietnamese language files for category management

---
+++ lang/vi/admin/category.php
@@ -0,0 +1,32 @@
+<?php
+return [
+    'plural' => 'Danh mục',
+    'singular' => 'Danh mục',
+
+    'cannot_hidden_category_has_children' => 'Không thể ẩn danh mục vì danh mục này có danh mục con.',
+    'manage_children_categories' => 'Quản lý danh mục con của :name',
+    'children_category' => 'Danh mục con',
+
+    'fields' => [
+        'name' => 'Tên danh mục',
+        'slug' => 'Đường dẫn tĩnh',
+        'image' => 'Hình ảnh',
+        'parents' => 'Danh mục cha',
+        'children' => 'Danh mục con',
+        'description' => 'Mô tả',
+        'created_at' => 'Ngày tạo',
+        'updated_at' => 'Ngày cập nhật',
+        'deleted_at' => 'Ngày xóa',
+    ],
+
+    'placeholders' => [
+        'name' => 'Nhập tên danh mục',
+        'slug' => 'Đường dẫn tĩnh được tạo tự động từ tên danh mục',
+        'description' => 'Nhập mô tả danh mục',
+    ],
+
+    'actions' => [
+        'create_child_category' => 'Tạo danh mục con',
+        'manage_children_categories' => 'Quản lý danh mục con',
+    ]
+];
