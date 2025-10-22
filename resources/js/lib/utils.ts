235b9495957a56a5193f8314a8582dafadd1021a
Peredar <64675226+perad4r@users.noreply.github.com>
2025-10-22 17:56:32 +0700

    feat: add csrf getting helper

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function csrf(): string {
+    return (document.querySelector('meta[name="csrf-token"]') as HTMLMetaElement)?.content || ''
+}
