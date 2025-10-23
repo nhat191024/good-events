80c70d1fd7a8e687e4c512ae2b545c0e6b1c9c0c
Fumino The Fish <richberchannel01@gmail.com>
2025-10-23 10:48:29 +0700

    feat: update navigation icon to OutlinedUserGroup for Partner and User resources

--- app/Filament/Admin/Resources/Partners/PartnerResource.php
+++ app/Filament/Admin/Resources/Partners/PartnerResource.php
@@ -28,7 +28,7 @@ class PartnerResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::UserGroup;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedUserGroup;
     protected static string|UnitEnum|null $navigationGroup = FilamentNavigationGroup::USER_MANAGEMENT;
 
     public static function getModelLabel(): string
