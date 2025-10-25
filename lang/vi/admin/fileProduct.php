9bfabe924051691c47ed48f709470d626eaae751
Fumino The Fish <richberchannel01@gmail.com>
2025-10-26 03:22:11 +0700

    feat: add English and Vietnamese language files for File Product

---
+++ lang/vi/admin/fileProduct.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'singular' => 'Thiết kế',
+
+    'fields' => [
+        'name' => 'Tên',
+        'category_id' => 'Danh mục',
+        'tags' => 'Thẻ',
+        'slug' => 'Đường dẫn tĩnh',
+        'description' => 'Mô tả',
+        'price' => 'Giá',
+        'thumbnail' => 'Hình xem trước',
+        'created_at' => 'Tạo lúc',
+        'updated_at' => 'Cập nhật lúc',
+        'deleted_at' => 'Xóa lúc',
+    ],
+
+    'placeholders' => [
+        'name' => 'Nhập tên thiết kế',
+        'tags' => 'Nhập tên thẻ để tìm kiếm và nhấn enter để thêm',
+        'slug' => 'Đường dẫn tĩnh sẽ được tạo tự động',
+        'description' => 'Nhập mô tả thiết kế',
+        'price' => 'Nhập giá thiết kế',
+    ],
+];
