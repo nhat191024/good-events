e481b851d2b685464d9a18d40b37014e93f1ac1d
Fumino The Fish <richberchannel01@gmail.com>
2025-10-31 14:40:22 +0700

    refactor: remove unused HasSlug trait from Tag model for cleaner code

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,14 +3,10 @@
 namespace App\Models;
 
 use Spatie\Tags\Tag as baseModel;
-
-use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 
 class Tag extends baseModel
 {
-    use HasSlug;
-
     /**
      * Get the options for generating the slug.
      */
