07a03bc0c148c1cae3bd0a39db299fa88843d4ef
Fumino The Fish <richberchannel01@gmail.com>
2025-11-02 12:39:29 +0700

    feat: add Vietnamese language support for password reset notifications

---
+++ lang/vi/passwords.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Mật khẩu của bạn đã được đặt lại!',
+    'sent' => 'Chúng tôi đã gửi liên kết đặt lại mật khẩu qua email!',
+    'throttled' => 'Vui lòng đợi trước khi thử lại.',
+    'token' => 'Mã đặt lại mật khẩu không hợp lệ.',
+    'user' => 'Chúng tôi không tìm thấy người dùng với địa chỉ email này.',
+
+    // Email content
+    'email' => [
+        'subject' => 'Đặt lại mật khẩu',
+        'greeting' => 'Xin chào!',
+        'line_1' => 'Bạn nhận được email này vì chúng tôi đã nhận được yêu cầu đặt lại mật khẩu cho tài khoản của bạn.',
+        'action' => 'Đặt lại mật khẩu',
+        'line_2' => 'Liên kết đặt lại mật khẩu này sẽ hết hạn sau :count phút.',
+        'line_3' => 'Nếu bạn không yêu cầu đặt lại mật khẩu, vui lòng bỏ qua email này.',
+        'salutation' => 'Trân trọng,',
+        'team_name' => 'Đội ngũ :app',
+        'subcopy' => 'Nếu bạn gặp sự cố khi nhấp vào nút ":action", hãy sao chép và dán URL bên dưới vào trình duyệt web của bạn:',
+    ],
+];
