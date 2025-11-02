0e73ed3caaec6d7c117637fd1a013fa593560144
Fumino The Fish <richberchannel01@gmail.com>
2025-11-02 12:39:29 +0700

    feat: add Vietnamese language support for password reset notifications

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Lang;
+
+class ResetPasswordNotification extends Notification
+{
+    use Queueable;
+
+    public string $token;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(string $token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $url = url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+
+        return (new MailMessage)
+            ->subject(Lang::get('passwords.email.subject'))
+            ->greeting(Lang::get('passwords.email.greeting'))
+            ->line(Lang::get('passwords.email.line_1'))
+            ->action(Lang::get('passwords.email.action'), $url)
+            ->line(Lang::get('passwords.email.line_2', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+            ->line(Lang::get('passwords.email.line_3'))
+            ->salutation(Lang::get('passwords.email.salutation') . "\n" . Lang::get('passwords.email.team_name', ['app' => config('app.name')]));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
