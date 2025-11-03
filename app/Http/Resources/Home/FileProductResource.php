dfdb64fc844570c116996320785b4f49ed15d19b
Peredar <64675226+perad4r@users.noreply.github.com>
2025-11-03 14:47:49 +0700

    feat: add FileProductResource for managing file product data representation

---
+++ app/Http/Resources/Home/FileProductResource.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Resources\Home;
+
+use App\Helper\TemporaryImage;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Log;
+
+/** @mixin \App\Models\FileProduct */
+class FileProductResource extends JsonResource
+{
+    public function toArray(Request $request)
+    {
+        $expireAt = now()->addMinutes(60 * 24);
+
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'price' => $this->price,
+            'image' => TemporaryImage::getTemporaryImageUrl($this, $expireAt, 'thumbnails'),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+
+            'category' => $this->whenLoaded('category', function () use ($expireAt) {
+                $cat = $this->category;
+
+                return [
+                    'id' => $cat->id,
+                    'name' => $cat->name,
+                    'slug' => $cat->slug,
+                    'description' => $cat->description,
+                    // 'image' => TemporaryImage::getTemporaryImageUrl($cat, $expireAt, 'thumbnails'),
+                    'parent' => $this->when(
+                        $cat->relationLoaded('parent') && $cat->parent,
+                        fn () => [
+                            'id' => $cat->parent->id,
+                            'name' => $cat->parent->name,
+                            // 'image' => $this->getTemporaryImageUrl($cat->parent, $expireAt),
+                        ]
+                    ),
+                ];
+            }),
+        ];
+    }
+}
