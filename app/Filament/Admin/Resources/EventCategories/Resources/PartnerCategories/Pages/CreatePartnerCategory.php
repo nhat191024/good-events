e2a9d9b7a5fad48746104e6b0bb9737c1fa2425f
Fumino The Fish <richberchannel01@gmail.com>
2025-11-04 17:01:30 +0700

    feat: implement breadcrumbs for CreatePartnerCategory page

--- app/Filament/Admin/Resources/EventCategories/Resources/PartnerCategories/Pages/CreatePartnerCategory.php
+++ app/Filament/Admin/Resources/EventCategories/Resources/PartnerCategories/Pages/CreatePartnerCategory.php
@@ -3,9 +3,20 @@
 namespace App\Filament\Admin\Resources\EventCategories\Resources\PartnerCategories\Pages;
 
 use App\Filament\Admin\Resources\EventCategories\Resources\PartnerCategories\PartnerCategoryResource;
+use App\Filament\Admin\Resources\EventCategories\EventCategoryResource;
+use App\Filament\Admin\Resources\EventCategories\Pages\ManagePartnerCategory;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreatePartnerCategory extends CreateRecord
 {
     protected static string $resource = PartnerCategoryResource::class;
+
+    public function getBreadcrumbs(): array
+    {
+        return [
+            EventCategoryResource::getIndexUrl() => __('admin/partnerCategory.plural'),
+            ManagePartnerCategory::getUrl([$this->getParentRecord()->id]) => $this->getParentRecord()->name,
+            __('global.create'),
+        ];
+    }
 }
