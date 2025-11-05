ec2057175b21212677a01db82799b16f03e8084c
Peredar <64675226+perad4r@users.noreply.github.com>
2025-11-05 15:42:01 +0700

    add: purchase page

---
+++ resources/js/pages/asset/Purchase.vue
@@ -0,0 +1,9 @@
+<template>
+    <PurchasePage v-bind="props" />
+</template>
+
+<script setup lang="ts">
+import PurchasePage, { type PurchasePageProps } from './purchase/index.vue';
+
+const props = defineProps<PurchasePageProps>();
+</script>
