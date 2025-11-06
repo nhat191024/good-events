bbed79849334286fc3a3c83a3d3d666411b9bfe2
Peredar <64675226+perad4r@users.noreply.github.com>
2025-11-06 12:36:05 +0700

    add: detail page index for blogs page

---
+++ resources/js/pages/blog/Detail.vue
@@ -0,0 +1,10 @@
+<template>
+    <DetailPage v-bind="props" />
+</template>
+
+<script setup lang="ts">
+import DetailPage, { type DetailPageProps } from './detail/index.vue';
+
+const props = defineProps<DetailPageProps>();
+</script>
+
