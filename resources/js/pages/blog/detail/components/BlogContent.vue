d17e139c3dcf52a64a30851e6ff1e34bebc35642
Peredar <64675226+perad4r@users.noreply.github.com>
2025-11-06 12:37:33 +0700

    add: blog content component

---
+++ resources/js/pages/blog/detail/components/BlogContent.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="flex flex-col gap-6">
+        <p v-if="blog.excerpt" class="text-lg font-medium text-gray-700">
+            {{ blog.excerpt }}
+        </p>
+
+        <div class="prose prose-lg max-w-none text-gray-700" v-html="blog.content" />
+    </section>
+</template>
+
+<script setup lang="ts">
+import type { BlogDetail } from '../../types';
+
+defineProps<{ blog: BlogDetail }>();
+</script>
+
