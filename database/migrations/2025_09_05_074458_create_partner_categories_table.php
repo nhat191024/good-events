e24795d1444309ac8692f4fe0191995060b5dbc6
Fumino The Fish <richberchannel01@gmail.com>
2025-11-06 19:05:54 +0700

    feat: add order property to Category and PartnerCategory models and migrations

--- database/migrations/2025_09_05_074458_create_partner_categories_table.php
+++ database/migrations/2025_09_05_074458_create_partner_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug')->unique();
+            $table->integer('order')->default(0);
             $table->foreignId('parent_id')->nullable()->constrained('partner_categories')->nullOnDelete();
             $table->double('min_price')->nullable();
             $table->double('max_price')->nullable();
