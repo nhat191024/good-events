58346d1d14d17baf6f2d46af6574c47f26dff7f0
Fumino The Fish <richberchannel01@gmail.com>
2025-11-15 18:13:31 +0700

    fix: rename banner fields to title fields in app settings

--- app/Settings/AppSettings.php
+++ app/Settings/AppSettings.php
@@ -11,9 +11,9 @@ class AppSettings extends Settings
     public string $app_name;
     public ?string $app_logo = null;
     public ?string $app_favicon = null;
-    public ?string $app_partner_banner = null;
-    public ?string $app_design_banner = null;
-    public ?string $app_rental_banner = null;
+    public ?string $app_partner_title = null;
+    public ?string $app_design_title = null;
+    public ?string $app_rental_title = null;
 
     public static function group(): string
     {
