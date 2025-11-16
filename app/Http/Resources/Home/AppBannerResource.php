38ac2c7c7e53e7aabb7cd60de3ced60ee4019732
Peredar <64675226+perad4r@users.noreply.github.com>
2025-11-16 18:39:06 +0700

    add: returning html tag directly, avoid complex frontend logic

---
+++ app/Http/Resources/Home/AppBannerResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources\Home;
+
+use App\Helper\TemporaryImage\TemporaryImage;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AppBannerResource extends JsonResource
+{
+    public function toArray(Request $request)
+    {
+        // $expireAt = now()->addMinutes(60 * 24);
+
+        return [
+            'image_tag' => $this->img('thumb')->attributes(['class' => 'w-full h-full object-cover'])->toHtml()
+        ];
+    }
+}
