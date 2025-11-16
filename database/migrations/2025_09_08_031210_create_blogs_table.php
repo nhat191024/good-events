f5cf423aa1e8791af70c9a283f45df59b75772dc
Fumino The Fish <richberchannel01@gmail.com>
2025-11-16 19:12:06 +0700

    feat: make blog content field nullable

--- database/migrations/2025_09_08_031210_create_blogs_table.php
+++ database/migrations/2025_09_08_031210_create_blogs_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('category_id')->constrained()->restrictOnDelete();
             $table->string('title');
-            $table->text('content');
+            $table->text('content')->nullable();
             $table->foreignId('user_id')->constrained()->restrictOnDelete();
             $table->string('slug');
             $table->string('video_url')->nullable();
