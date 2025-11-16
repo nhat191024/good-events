d416fbf0f1dbc6f1d3b3bac4ebee5aed2e112c09
Peredar <64675226+perad4r@users.noreply.github.com>
2025-11-16 21:42:36 +0700

    feat: translate tag name and slug in TagResource

--- app/Http/Resources/Home/TagResource.php
+++ app/Http/Resources/Home/TagResource.php
@@ -5,24 +5,17 @@ namespace App\Http\Resources\Home;
 use App\Helper\TemporaryImage\TemporaryImage;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Log;
 
 /** @mixin \App\Models\Tag */
 class TagResource extends JsonResource
 {
     public function toArray(Request $request)
     {
-        $expireAt = now()->addMinutes(60 * 24);
-
         return [
             'id' => $this->id,
-            'name' => $this->name,
-            'slug' => $this->slug,
-            // 'description' => $this->description,
-            // 'price' => $this->price,
-            // 'image' => $this->getTemporaryImageUrl($this, $expireAt),
-            // 'created_at' => $this->created_at,
-            // 'updated_at' => $this->updated_at,
-
+            'name' => $this->getTranslation('name', 'vi'),
+            'slug' => $this->getTranslation('slug', 'vi'),
         ];
     }
 }
