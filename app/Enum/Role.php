1336c446c08261a0737b815ede33dd4706e68314
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-11-20 04:55:13 +0700

    feat: add additional roles and update label method in Role enum

--- app/Enum/Role.php
+++ app/Enum/Role.php
@@ -8,4 +8,20 @@ enum Role: string
     case ADMIN = 'admin';
     case PARTNER = 'partner';
     case CLIENT = 'client';
+    case HUMAN_RESOURCE_MANAGER = 'human_resource_manager';
+    case DESIGN_MANAGER = 'design_manager';
+    case RENTAL_MANAGER = 'rental_manager';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::SUPER_ADMIN => __('global.role.super_admin'),
+            self::ADMIN => __('global.role.admin'),
+            self::PARTNER => __('global.role.partner'),
+            self::CLIENT => __('global.role.client'),
+            self::HUMAN_RESOURCE_MANAGER => __('global.role.human_resource_manager'),
+            self::DESIGN_MANAGER => __('global.role.design_manager'),
+            self::RENTAL_MANAGER => __('global.role.rental_manager'),
+        };
+    }
 }
