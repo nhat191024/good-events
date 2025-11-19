baa466410695a8f1b51f316647191bd66b8c22e4
Peredar <64675226+perad4r@users.noreply.github.com>
2025-11-19 21:09:29 +0700

    feat: add type and video URL to blog resource response

--- app/Http/Resources/Home/BlogResource.php
+++ app/Http/Resources/Home/BlogResource.php
@@ -19,10 +19,12 @@ class BlogResource extends JsonResource
             'id' => $this->id,
             'title' => $this->title,
             'slug' => $this->slug,
+            'type' => $this->type,
             'excerpt' => $plainContent->limit(140)->toString(),
             'published_at' => optional($this->created_at)->toIso8601String(),
             'published_human' => optional($this->created_at)->translatedFormat('d M Y'),
             'thumbnail' => TemporaryImage::getTemporaryImageUrl($this, $expireAt, 'thumbnail'),
+            'video_url' => $this->video_url,
             'author' => $this->whenLoaded('author', fn () => [
                 'id' => $this->author->id,
                 'name' => $this->author->name,
