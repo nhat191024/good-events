8fa6a92e28d20f118ee5246c8a68aa8b5e240092
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-11-20 05:04:26 +0700

    feat: add additional roles to RoleSeeder for user management

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -19,5 +19,8 @@ class RoleSeeder extends Seeder
         Role::create(['name' => 'admin']);
         Role::create(['name' => 'partner']);
         Role::create(['name' => 'client']);
+        Role::create(['name' => 'human_resource_manager']);
+        Role::create(['name' => 'design_manager']);
+        Role::create(['name' => 'rental_manager']);
     }
 }
