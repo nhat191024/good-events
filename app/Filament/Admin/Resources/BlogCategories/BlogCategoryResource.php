766d7e0914302be553280bca0b4a57ce45c84cb0
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-11-20 06:08:29 +0700

    feat: create BlogCategory model extending Category and update BlogCategoryResource to use it

--- app/Filament/Admin/Resources/BlogCategories/BlogCategoryResource.php
+++ app/Filament/Admin/Resources/BlogCategories/BlogCategoryResource.php
@@ -2,7 +2,7 @@
 
 namespace App\Filament\Admin\Resources\BlogCategories;
 
-use App\Models\Category;
+use App\Models\BlogCategory;
 
 use BackedEnum;
 use UnitEnum;
@@ -28,7 +28,7 @@ use App\Enum\FilamentNavigationGroup;
 
 class BlogCategoryResource extends Resource
 {
-    protected static ?string $model = Category::class;
+    protected static ?string $model = BlogCategory::class;
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
     protected static string|UnitEnum|null $navigationGroup = FilamentNavigationGroup::CATEGORIES;
