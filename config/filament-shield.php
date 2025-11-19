742e66e558a0207e4cf35584cc859e55f732f620
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-11-20 06:13:41 +0700

    feat: disable widgets tab in Shield resource configuration

--- config/filament-shield.php
+++ config/filament-shield.php
@@ -19,7 +19,7 @@ return [
         'cluster' => null,
         'tabs' => [
             'pages' => true,
-            'widgets' => true,
+            'widgets' => false,
             'resources' => true,
             'custom_permissions' => false,
         ],
