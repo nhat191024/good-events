972c40e2be2651c17ea26d8b4064adbaf4fb4c01
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-11-21 09:28:39 +0700

    feat: implement QR_TRANSFER payment method with label, description, and gateway channel

--- app/Enum/PaymentMethod.php
+++ app/Enum/PaymentMethod.php
@@ -4,16 +4,39 @@ namespace App\Enum;
 
 enum PaymentMethod: string
 {
-    //only bank transfer available for now
-    case BANK_TRANSFER = 'bank_transfer';
-    // case PAYPAL = 'paypal';
-    // case CREDIT_CARD = 'credit_card';
-    // case CASH = 'cash';
+    case QR_TRANSFER = 'qr_transfer';
 
     public function label(): string
     {
         return match ($this) {
-            self::BANK_TRANSFER => __('admin/fileProductBill.status.bank_transfer'),
+            self::QR_TRANSFER => __('admin/fileProductBill.payment_method.qr_transfer'),
         };
+    }
+
+    public function description(): ?string
+    {
+        return match ($this) {
+            self::QR_TRANSFER => __('admin/fileProductBill.description.qr_transfer'),
+        };
+    }
+
+    public function gatewayChannel(): string
+    {
+        return match ($this) {
+            self::QR_TRANSFER => 'qr_transfer',
+        };
+    }
+
+    /**
+     * Return an array suitable for front-end selection list
+     * @return array<int, array{code:string,name:string,description:string|null}>
+     */
+    public static function toOptions(): array
+    {
+        return array_map(fn (self $value) => [
+            'code' => $value->value,
+            'name' => $value->label(),
+            'description' => $value->description(),
+        ], self::cases());
     }
 }
