9f0d84d2a4cb6209568e42b4e2d850144848a6dd
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-11-21 10:09:16 +0700

    refactor: simplify image URL retrieval in FileProductResource

--- app/Http/Resources/Home/FileProductResource.php
+++ app/Http/Resources/Home/FileProductResource.php
@@ -12,19 +12,17 @@ class FileProductResource extends JsonResource
 {
     public function toArray(Request $request)
     {
-        $expireAt = now()->addMinutes(200 * 24);
-
         return [
             'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
             'description' => $this->description,
             'price' => $this->price,
-            'image' => TemporaryImage::getTemporaryImageUrl($this, $expireAt, 'thumbnails'),
+            'image' => $this->getFirstMediaUrl('thumbnails'),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
 
-            'category' => $this->whenLoaded('category', function () use ($expireAt) {
+            'category' => $this->whenLoaded('category', function () {
                 $cat = $this->category;
 
                 return [
