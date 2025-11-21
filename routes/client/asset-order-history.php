fca4401a18b5f49c0f2785db89c6f3c4f225880f
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-11-21 13:41:20 +0700

    feat: add download, downloadAll, and downloadZip routes for asset orders

--- routes/client/asset-order-history.php
+++ routes/client/asset-order-history.php
@@ -18,4 +18,19 @@ Route::prefix('tai-lieu')->name('asset.')->group(function () {
         '/{bill}/repay',
         [AssetOrderController::class, 'repay']
     )->whereNumber('bill')->name('repay');
+
+    Route::get(
+        '/{bill}/download',
+        [AssetOrderController::class, 'download']
+    )->whereNumber('bill')->name('download');
+
+    Route::get(
+        '/{bill}/download/all',
+        [AssetOrderController::class, 'downloadAll']
+    )->whereNumber('bill')->name('downloadAll');
+
+    Route::get(
+        '/{bill}/download/zip',
+        [AssetOrderController::class, 'downloadZip']
+    )->whereNumber('bill')->name('downloadZip');
 });
