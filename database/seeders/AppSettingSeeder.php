d2bd2e251ae7e5106c7a5ae47935a5445f437157
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-11-22 17:29:32 +0700

    feat: Add application settings for partner titles and contact information

--- database/seeders/AppSettingSeeder.php
+++ database/seeders/AppSettingSeeder.php
@@ -21,6 +21,11 @@ class AppSettingSeeder extends Seeder
         $settings->app_name = 'Sự Kiện Tốt';
         $settings->app_logo = 'images/logo.svg';
         $settings->app_favicon = 'images/favicon.ico';
+        $settings->app_partner_title = 'Đối tác của chúng tôi';
+        $settings->app_design_title = 'Dịch vụ thiết kế sự kiện';
+        $settings->app_rental_title = 'Dịch vụ cho thuê thiết bị sự kiện';
+        $settings->contact_hotline = '1800 1234';
+        $settings->contact_email = 'contact@sukientot.com';
         $settings->save();
     }
 }
