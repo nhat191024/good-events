48d7bef8b9673c1e4650a23e3b0173f848fa383c
Peredar <64675226+perad4r@users.noreply.github.com>
2025-11-26 14:03:59 +0700

    update: add routes for new tutorial page

--- routes/static-pages.php
+++ routes/static-pages.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/ve-chung-toi', [AboutController::class, 'index'])->name('about.index');
 Route::get('/lien-he', [ContactController::class, 'index'])->name('contact.index');
+Route::get('/huong-dan', [App\Http\Controllers\TutorialController::class, 'index'])->name('tutorial.index');
+Route::get('/huong-dan/khac', [App\Http\Controllers\TutorialController::class, 'other'])->name('tutorial.other');
