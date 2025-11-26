e0b0e6a5b34e772cdc7c45d4781df854b060b8db
Peredar <64675226+perad4r@users.noreply.github.com>
2025-11-26 17:45:57 +0700

    style: adjust grid columns for category children on mobile

--- resources/js/pages/home/CategoryChildren.vue
+++ resources/js/pages/home/CategoryChildren.vue
@@ -22,7 +22,7 @@
                 />
             </div>
 
-            <div v-if="filteredCategories.length" class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div v-if="filteredCategories.length" class="grid grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                 <PartnerCategoryCard v-for="partnerCategory in filteredCategories" :key="partnerCategory.id"
                     :partner-category="partnerCategory" />
             </div>
