e887aa750b4046ad993082c13fd7e7f209fd7c18
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-11-28 16:19:23 +0700

    feat: create ListLogs page with page shield integration

---
+++ app/Filament/Admin/Pages/ListLogs.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Admin\Pages;
+
+use Boquizo\FilamentLogViewer\Pages\ListLogs as BaseListLogs;
+
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
+
+class ListLogs extends BaseListLogs
+{
+    use HasPageShield;
+}
+
+
