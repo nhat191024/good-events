a796282d78d6e0ab3131a604af9c425f70eb4f06
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-11-28 15:36:35 +0700

    feat: update form UI

--- app/Filament/Admin/Resources/EventOrganizationGuides/Schemas/EventOrganizationGuideForm.php
+++ app/Filament/Admin/Resources/EventOrganizationGuides/Schemas/EventOrganizationGuideForm.php
@@ -4,6 +4,8 @@ namespace App\Filament\Admin\Resources\EventOrganizationGuides\Schemas;
 
 use App\Models\Category;
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Grid;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Select;
@@ -17,57 +19,84 @@ class EventOrganizationGuideForm
     {
         return $schema
             ->components([
-                TextInput::make('title')
-                    ->label(__('admin/blog.fields.title'))
-                    ->placeholder(__('admin/blog.placeholders.title'))
-                    ->columnSpanFull()
-                    ->required(),
-                Select::make('category_id')
-                    ->label(__('admin/blog.fields.category_id'))
-                    ->searchable()
-                    ->options(
-                        fn() => Category::query()
-                            ->whereType('event_organization_guide')
-                            ->orderBy('created_at', 'desc')
-                            ->limit(10)
-                            ->pluck('name', 'id')
-                            ->toArray()
-                    )
-                    ->getSearchResultsUsing(
-                        fn(string $search): array =>
-                        Category::query()
-                            ->whereType('event_organization_guide')
-                            ->where('name', 'like', "%{$search}%")
-                            ->limit(50)
-                            ->pluck('name', 'id')
-                            ->toArray()
-                    )
-                    ->getOptionLabelUsing(
-                        fn($value): ?string =>
-                        Category::find($value)?->name
-                    )
-                    ->required(),
-                TextInput::make('slug')
-                    ->label(__('admin/blog.fields.slug'))
-                    ->placeholder(__('admin/blog.placeholders.slug'))
-                    ->disabled(),
-                SpatieTagsInput::make('tags')
-                    ->label(__('admin/blog.fields.tags'))
-                    ->placeholder(__('admin/blog.placeholders.tags'))
-                    ->required()
+
+                Section::make(__('admin/blog.sections.basic_information'))
+                    ->description(__('admin/blog.sections.descriptions.basic_information'))
+                    ->icon('heroicon-o-information-circle')
+                    ->collapsible()
+                    ->schema([
+                        TextInput::make('title')
+                            ->label(__('admin/blog.fields.title'))
+                            ->placeholder(__('admin/blog.placeholders.title'))
+                            ->required(),
+                        Grid::make(2)
+                            ->schema([
+                                Select::make('category_id')
+                                    ->label(__('admin/blog.fields.category_id'))
+                                    ->searchable()
+                                    ->options(
+                                        fn() => Category::query()
+                                            ->whereType('event_organization_guide')
+                                            ->orderBy('created_at', 'desc')
+                                            ->limit(10)
+                                            ->pluck('name', 'id')
+                                            ->toArray()
+                                    )
+                                    ->getSearchResultsUsing(
+                                        fn(string $search): array =>
+                                        Category::query()
+                                            ->whereType('event_organization_guide')
+                                            ->where('name', 'like', "%{$search}%")
+                                            ->limit(50)
+                                            ->pluck('name', 'id')
+                                            ->toArray()
+                                    )
+                                    ->getOptionLabelUsing(
+                                        fn($value): ?string =>
+                                        Category::find($value)?->name
+                                    )
+                                    ->required(),
+                                TextInput::make('slug')
+                                    ->label(__('admin/blog.fields.slug'))
+                                    ->placeholder(__('admin/blog.placeholders.slug'))
+                                    ->disabled(),
+                            ]),
+                        SpatieTagsInput::make('tags')
+                            ->label(__('admin/blog.fields.tags'))
+                            ->placeholder(__('admin/blog.placeholders.tags'))
+                            ->required(),
+                        TextInput::make('video_url')
+                            ->label(__('admin/blog.fields.video_url'))
+                            ->placeholder(__('admin/blog.placeholders.video_url'))
+                            ->url(),
+                    ])
                     ->columnSpanFull(),
-                RichEditor::make('content')
-                    ->label(__('admin/blog.fields.content'))
-                    ->placeholder(__('admin/blog.placeholders.content'))
-                    ->columnSpanFull()
-                    ->required(),
-                SpatieMediaLibraryFileUpload::make('images')
-                    ->label(__('admin/blog.fields.thumbnail'))
-                    ->collection('thumbnail')
-                    ->required()
-                    ->image()
-                    ->imageEditor()
-                    ->maxFiles(1)
+
+                Section::make(__('admin/blog.sections.content'))
+                    ->description(__('admin/blog.sections.content_description'))
+                    ->icon('heroicon-o-document-text')
+                    ->collapsible()
+                    ->schema([
+                        RichEditor::make('content')
+                            ->label(__('admin/blog.fields.content'))
+                            ->placeholder(__('admin/blog.placeholders.content'))
+                            ->required(),
+                    ])
+                    ->columnSpanFull(),
+
+                Section::make(__('admin/blog.sections.media'))
+                    ->description(__('admin/blog.sections.media_description'))
+                    ->icon('heroicon-o-photo')
+                    ->collapsible()
+                    ->schema([
+                        SpatieMediaLibraryFileUpload::make('images')
+                            ->label(__('admin/blog.fields.thumbnail'))
+                            ->collection('thumbnail')
+                            ->required()
+                            ->image()
+                            ->imageEditor()
+                            ->maxFiles(1),
+                    ])
                     ->columnSpanFull(),
             ]);
     }
--- app/Filament/Admin/Resources/VocationalKnowledge/Schemas/VocationalKnowledgeForm.php
+++ app/Filament/Admin/Resources/VocationalKnowledge/Schemas/VocationalKnowledgeForm.php
@@ -5,9 +5,12 @@ namespace App\Filament\Admin\Resources\VocationalKnowledge\Schemas;
 use App\Models\Category;
 
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Grid;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Filament\Forms\Components\SpatieTagsInput;
 
@@ -17,57 +20,84 @@ class VocationalKnowledgeForm
     {
         return $schema
             ->components([
-                TextInput::make('title')
-                    ->label(__('admin/blog.fields.title'))
-                    ->placeholder(__('admin/blog.placeholders.title'))
-                    ->required(),
-                TextInput::make('slug')
-                    ->label(__('admin/blog.fields.slug'))
-                    ->placeholder(__('admin/blog.placeholders.slug'))
-                    ->disabled(),
-                TextInput::make('video_url')
-                    ->label(__('admin/blog.fields.video_url'))
-                    ->placeholder(__('admin/blog.placeholders.video_url'))
-                    ->url()
-                    ->required(),
-                Select::make('category_id')
-                    ->label(__('admin/blog.fields.category_id'))
-                    ->searchable()
-                    ->options(
-                        fn() => Category::query()
-                            ->whereType('vocational_knowledge')
-                            ->orderBy('created_at', 'desc')
-                            ->limit(10)
-                            ->pluck('name', 'id')
-                            ->toArray()
-                    )
-                    ->getSearchResultsUsing(
-                        fn(string $search): array =>
-                        Category::query()
-                            ->whereType('vocational_knowledge')
-                            ->where('name', 'like', "%{$search}%")
-                            ->limit(50)
-                            ->pluck('name', 'id')
-                            ->toArray()
-                    )
-                    ->getOptionLabelUsing(
-                        fn($value): ?string =>
-                        Category::find($value)?->name
-                    )
-                    ->required(),
 
-                SpatieTagsInput::make('tags')
-                    ->label(__('admin/blog.fields.tags'))
-                    ->placeholder(__('admin/blog.placeholders.tags'))
-                    ->required()
+                Section::make(__('admin/blog.sections.basic_info'))
+                    ->description(__('admin/blog.sections.basic_info_description'))
+                    ->icon('heroicon-o-information-circle')
+                    ->collapsible()
+                    ->schema([
+                        Grid::make(2)
+                            ->schema([
+                                TextInput::make('title')
+                                    ->label(__('admin/blog.fields.title'))
+                                    ->placeholder(__('admin/blog.placeholders.title'))
+                                    ->required(),
+                                TextInput::make('slug')
+                                    ->label(__('admin/blog.fields.slug'))
+                                    ->placeholder(__('admin/blog.placeholders.slug'))
+                                    ->disabled(),
+                                TextInput::make('video_url')
+                                    ->label(__('admin/blog.fields.video_url'))
+                                    ->placeholder(__('admin/blog.placeholders.video_url'))
+                                    ->url(),
+                                Select::make('category_id')
+                                    ->label(__('admin/blog.fields.category_id'))
+                                    ->searchable()
+                                    ->options(
+                                        fn() => Category::query()
+                                            ->whereType('vocational_knowledge')
+                                            ->orderBy('created_at', 'desc')
+                                            ->limit(10)
+                                            ->pluck('name', 'id')
+                                            ->toArray()
+                                    )
+                                    ->getSearchResultsUsing(
+                                        fn(string $search): array =>
+                                        Category::query()
+                                            ->whereType('vocational_knowledge')
+                                            ->where('name', 'like', "%{$search}%")
+                                            ->limit(50)
+                                            ->pluck('name', 'id')
+                                            ->toArray()
+                                    )
+                                    ->getOptionLabelUsing(
+                                        fn($value): ?string =>
+                                        Category::find($value)?->name
+                                    )
+                                    ->required(),
+                            ]),
+                        SpatieTagsInput::make('tags')
+                            ->label(__('admin/blog.fields.tags'))
+                            ->placeholder(__('admin/blog.placeholders.tags'))
+                            ->required(),
+                    ])
                     ->columnSpanFull(),
-                SpatieMediaLibraryFileUpload::make('images')
-                    ->label(__('admin/blog.fields.thumbnail'))
-                    ->collection('thumbnail')
-                    ->required()
-                    ->image()
-                    ->imageEditor()
-                    ->maxFiles(1)
+
+                Section::make(__('admin/blog.sections.content'))
+                    ->description(__('admin/blog.sections.content_description'))
+                    ->icon('heroicon-o-document-text')
+                    ->collapsible()
+                    ->schema([
+                        RichEditor::make('content')
+                            ->label(__('admin/blog.fields.content'))
+                            ->placeholder(__('admin/blog.placeholders.content'))
+                            ->required(),
+                    ])
+                    ->columnSpanFull(),
+
+                Section::make(__('admin/blog.sections.media'))
+                    ->description(__('admin/blog.sections.media_description'))
+                    ->icon('heroicon-o-photo')
+                    ->collapsible()
+                    ->schema([
+                        SpatieMediaLibraryFileUpload::make('images')
+                            ->label(__('admin/blog.fields.thumbnail'))
+                            ->collection('thumbnail')
+                            ->required()
+                            ->image()
+                            ->imageEditor()
+                            ->maxFiles(1),
+                    ])
                     ->columnSpanFull(),
             ]);
     }
