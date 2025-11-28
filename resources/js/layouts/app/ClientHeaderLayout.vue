712134ce01bebd56771cd585aa249de5ad907fa5
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-11-29 01:50:28 +0700

    reformat

--- resources/js/layouts/app/ClientHeaderLayout.vue
+++ resources/js/layouts/app/ClientHeaderLayout.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import AppContent from '@/components/AppContent.vue';
-import Header from '@/pages/home/partials/Header.vue';
-import ConfirmModal from '@/components/ConfirmModal.vue'
+import ConfirmModal from '@/components/ConfirmModal.vue';
 import Loading from '@/components/Loading.vue';
-import type { BreadcrumbItemType } from '@/types';
 import Footer from '@/pages/home/partials/Footer.vue';
-import { Head } from '@inertiajs/vue3';
+import Header from '@/pages/home/partials/Header.vue';
+import type { BreadcrumbItemType } from '@/types';
 import { onMounted } from 'vue';
 
 interface Props {
@@ -24,14 +23,13 @@ withDefaults(defineProps<Props>(), {
 });
 
 onMounted(() => {
-    document.documentElement.classList.remove('dark')
-})
-
+    document.documentElement.classList.remove('dark');
+});
 </script>
 
 <template>
     <Header v-if="showNav" :background-class-names="backgroundClassNames" />
-    <main :class="showNav?'pt-16':'pt-0'">
+    <main :class="showNav ? 'pt-16' : 'pt-0'">
         <div class="h-full bg-white">
             <AppContent>
                 <!-- the red bg banner on top of the page -->
