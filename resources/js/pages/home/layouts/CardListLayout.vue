990f1ec94a67e9ce54e08a308a21ab54fd43ad0e
Peredar <64675226+perad4r@users.noreply.github.com>
2025-12-03 16:37:27 +0700

    feat: remove swiper implementation

--- resources/js/pages/home/layouts/CardListLayout.vue
+++ resources/js/pages/home/layouts/CardListLayout.vue
@@ -10,16 +10,11 @@
                 </h2>
             </Link>
 
-            <Swiper v-if="items.length" :modules="[Navigation, Autoplay]" :slides-per-view="1.25" :space-between="8"
-                :navigation="true" :autoplay="autoplayOptions" :loop="items.length > 3"
-                :breakpoints="swiperBreakpoints"
-                class="group/card-swiper card-list-swiper home-swiper-nav !pb-2"
-                style="--swiper-nav-right-mobile: 4px; --swiper-nav-right-desktop: 4px; --swiper-nav-top: 120px; --swiper-nav-translate: none;"
-                @reachEnd="handleReachEnd">
-                <SwiperSlide v-for="(item, index) in items" :key="getItemKey(item, index)" class="!h-auto">
-                    <slot :item="item" :index="index" />
-                </SwiperSlide>
-            </Swiper>
+            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 md:gap-4 lg:gap-6 gap-2">
+                <div v-for="item in items" :key="getItemKey(item)">
+                    <slot :item="item" />
+                </div>
+            </div>
         </motion.section>
     </div>
 </template>
@@ -27,12 +22,7 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import { Link } from '@inertiajs/vue3';
-// import { Swiper, SwiperSlide } from 'swiper/vue';
-// import { Navigation, Autoplay } from 'swiper/modules';
 import { motion } from 'motion-v';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import '../styles/swiper-nav.css';
 import { cn } from '@/lib/utils';
 
 interface Props {
@@ -42,29 +32,11 @@ interface Props {
     items: unknown[];
 }
 
-const emit = defineEmits<{
-    (e: 'reach-end'): void;
-}>();
-
 const props = withDefaults(defineProps<Props>(), {
     items: () => [],
 });
 
 const items = computed(() => props.items ?? []);
-
-const swiperBreakpoints = {
-    640: { slidesPerView: 2, spaceBetween: 12 },
-    768: { slidesPerView: 3, spaceBetween: 16 },
-    1024: { slidesPerView: 4, spaceBetween: 20 },
-};
-
-const autoplayOptions = {
-    delay: 4500,
-    disableOnInteraction: false,
-    pauseOnMouseEnter: true,
-};
-
-const handleReachEnd = () => emit('reach-end');
 
 const getItemKey = (item: any, index: number) => item?.id ?? item?.slug ?? item?.key ?? index;
 
