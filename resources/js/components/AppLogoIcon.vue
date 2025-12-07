95d58199fa262d77f76c810f6830b3f57f09ed8c
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-07 15:01:57 +0700

    fix: standardize formatting in AppLogoIcon.vue

--- resources/js/components/AppLogoIcon.vue
+++ resources/js/components/AppLogoIcon.vue
@@ -1,23 +1,23 @@
 <script setup lang="ts">
-    import { getImg } from '@/pages/booking/helper';
-    import { AppSettings } from '@/types';
-    import { usePage } from '@inertiajs/vue3';
-    import { computed, type HTMLAttributes } from 'vue';
+import { getImg } from '@/pages/booking/helper';
+import { AppSettings } from '@/types';
+import { usePage } from '@inertiajs/vue3';
+import { computed, type HTMLAttributes } from 'vue';
 
-    const page = usePage()
-    const settings = computed(() => page.props.app_settings as AppSettings)
+const page = usePage();
+const settings = computed(() => page.props.app_settings as AppSettings);
 
-    defineOptions({
-        inheritAttrs: false,
-    });
+defineOptions({
+    inheritAttrs: false,
+});
 
-    interface Props {
-        className?: HTMLAttributes['class'];
-    }
+interface Props {
+    className?: HTMLAttributes['class'];
+}
 
-    defineProps<Props>();
+defineProps<Props>();
 </script>
 
 <template>
-    <img :src="getImg(`/${settings.app_logo}`)" alt="Sukientot" class="h-9 w-9 rounded-full object-contain ring-2 ring-white/40" />
+    <img :src="getImg(`${settings.app_logo}`)" alt="Sukientot" class="h-9 w-9 rounded-full object-contain ring-2 ring-white/40" />
 </template>
