012c62e03e651565476930066ac1b0c74cb80cfe
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-13 12:28:40 +0700

    fix: add missing role 'super_admin' to RoleSeeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -16,6 +16,7 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
+        Role::create(['name' => 'super_admin']);
         Role::create(['name' => 'admin']);
         Role::create(['name' => 'partner']);
         Role::create(['name' => 'client']);
