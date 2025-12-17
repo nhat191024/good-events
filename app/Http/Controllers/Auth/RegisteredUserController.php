e7fd1432e673e5dccdc185bbb6dc1f96b43000ca
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-17 10:41:23 +0700

    fix: update user creation logic to use Customer and Partner models, and adjust balance handling for partners

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -19,9 +19,11 @@ use App\Settings\PartnerSettings;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
+
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+
 use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\Rules;
 
