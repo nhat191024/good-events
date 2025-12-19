f1eaeca42a63f3fd85506a22497ff87396f80e9f
Peredar <64675226+perad4r@users.noreply.github.com>
2025-12-19 13:19:19 +0700

    feat: enhance sidebar navigation by adding partner registration option

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,38 +1,43 @@
 <script setup lang="ts">
-    import NavFooter from '@/components/NavFooter.vue';
-    import NavMain from '@/components/NavMain.vue';
-    import NavUser from '@/components/NavUser.vue';
-    import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
-    import { type NavItem } from '@/types';
-    import { Link } from '@inertiajs/vue3';
-    import { Lock, UserCircle } from 'lucide-vue-next';
-    import AppLogo from './AppLogo.vue';
+import NavFooter from '@/components/NavFooter.vue';
+import NavMain from '@/components/NavMain.vue';
+import NavUser from '@/components/NavUser.vue';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { type NavItem } from '@/types';
+import { Link } from '@inertiajs/vue3';
+import { Lock, UserCircle, Briefcase } from 'lucide-vue-next';
+import AppLogo from './AppLogo.vue';
 
-    const mainNavItems: NavItem[] = [
-        {
-            title: 'Tổng quan',
-            href: route('profile.edit'),
-            icon: UserCircle,
-        },
-        {
-            title: 'Bảo mật',
-            href: route('profile.password.edit'),
-            icon: Lock,
-        },
-    ];
+const mainNavItems: NavItem[] = [
+    {
+        title: 'Tổng quan',
+        href: route('profile.edit'),
+        icon: UserCircle,
+    },
+    {
+        title: 'Bảo mật',
+        href: route('profile.password.edit'),
+        icon: Lock,
+    },
+    {
+        title: 'Đăng ký làm nhân sự',
+        href: route('partner.register.create'),
+        icon: Briefcase,
+    },
+];
 
-    const footerNavItems: NavItem[] = [
-        // {
-        //     title: 'Github Repo',
-        //     href: 'https://github.com/laravel/vue-starter-kit',
-        //     icon: Folder,
-        // },
-        // {
-        //     title: 'Documentation',
-        //     href: 'https://laravel.com/docs/starter-kits#vue',
-        //     icon: BookOpen,
-        // },
-    ];
+const footerNavItems: NavItem[] = [
+    // {
+    //     title: 'Github Repo',
+    //     href: 'https://github.com/laravel/vue-starter-kit',
+    //     icon: Folder,
+    // },
+    // {
+    //     title: 'Documentation',
+    //     href: 'https://laravel.com/docs/starter-kits#vue',
+    //     icon: BookOpen,
+    // },
+];
 </script>
 
 <template>
