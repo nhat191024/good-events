4ba9d9fca6472d6579d1ef6b5144f7cb40c7021b
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-20 18:32:02 +0700

    feat: implement caching for partner services in PartnerService model

--- app/Enum/CacheKey.php
+++ app/Enum/CacheKey.php
@@ -7,4 +7,5 @@ enum CacheKey: string
     case PARTNER_CATEGORIES = 'partner_categories';
     case PARTNER_CATEGORIES_TREE = 'partner_categories_tree';
     case PARTNER_CATEGORIES_ALL = 'partner_categories_all';
+    case PARTNER_SERVICES = 'partner_services';
 }
