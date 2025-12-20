e496d96595e6a8f3fede877548937bb0acb46e06
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-20 18:20:56 +0700

    feat: add CacheKey enum for partner categories

---
+++ app/Enum/CacheKey.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enum;
+
+enum CacheKey: string
+{
+    case PARTNER_CATEGORIES = 'partner_categories';
+    case PARTNER_CATEGORIES_TREE = 'partner_categories_tree';
+    case PARTNER_CATEGORIES_ALL = 'partner_categories_all';
+}
