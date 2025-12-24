01df4a9ead76245003730759abe950133ca6b2b5
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-24 16:24:45 +0700

    feat: add google service configuration to third-party services

--- config/services.php
+++ config/services.php
@@ -39,4 +39,9 @@ return [
         'key' => env('OPEN_CAGE_API_KEY'),
     ],
 
+    'google' => [
+        'client_id'     => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect'      => env('GOOGLE_REDIRECT_URI'),
+    ],
 ];
