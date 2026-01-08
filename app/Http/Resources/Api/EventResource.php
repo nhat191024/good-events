890d2142a9fc96818bcd27da1e4dffbbe03d3bab
Peredar <64675226+perad4r@users.noreply.github.com>
2026-01-08 13:54:20 +0700

    add: app/Http/Resources/Api/EventResource.php

---
+++ app/Http/Resources/Api/EventResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources\Api;
+
+use Illuminate\Http\Request;
+
+/** @mixin \App\Models\Event */
+class EventResource extends BaseResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'created_at' => optional($this->created_at)->toIso8601String(),
+            'updated_at' => optional($this->updated_at)->toIso8601String(),
+        ];
+    }
+}
