c6fad7d958ef76f31f1c905ac63007d94a6fcd68
Peredar <64675226+perad4r@users.noreply.github.com>
2026-01-08 13:53:56 +0700

    update: api location route

--- routes/api/location.php
+++ routes/api/location.php
@@ -1,6 +1,23 @@
 <?php
 use App\Http\Controllers\Api\LocationController;
+use App\Http\Controllers\Api\ResourceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/locations/{location}/wards', [LocationController::class, 'wards'])
     ->middleware('throttle:api');
+
+Route::middleware(['auth:sanctum', 'throttle:api'])->group(function () {
+    Route::get('/locations', [ResourceController::class, 'index'])
+        ->defaults('resource', 'locations');
+    Route::get('/locations/{id}', [ResourceController::class, 'show'])
+        ->whereNumber('id')
+        ->defaults('resource', 'locations');
+    Route::post('/locations', [ResourceController::class, 'store'])
+        ->defaults('resource', 'locations');
+    Route::post('/locations/{id}', [ResourceController::class, 'update'])
+        ->whereNumber('id')
+        ->defaults('resource', 'locations');
+    Route::post('/locations/{id}/delete', [ResourceController::class, 'destroy'])
+        ->whereNumber('id')
+        ->defaults('resource', 'locations');
+});
