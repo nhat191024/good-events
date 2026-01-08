1993ec0504e9f8251f4a844227ed87189de3a04e
Peredar <64675226+perad4r@users.noreply.github.com>
2026-01-08 13:54:21 +0700

    add: app/Http/Resources/Api/UserResource.php

---
+++ app/Http/Resources/Api/UserResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\Api;
+
+use Illuminate\Http\Request;
+
+/** @mixin \App\Models\User */
+class UserResource extends BaseResource
+{
+    public function toArray(Request $request): array
+    {
+        $partnerProfile = $this->whenLoaded('partnerProfile');
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'country_code' => $this->country_code,
+            'bio' => $this->bio,
+            'avatar' => $this->avatar_url,
+            'email_verified_at' => optional($this->email_verified_at)->toIso8601String(),
+            'created_at' => optional($this->created_at)->toIso8601String(),
+            'updated_at' => optional($this->updated_at)->toIso8601String(),
+            'roles' => $this->whenLoaded('roles', fn () => $this->roles->pluck('name')->values()),
+            'partner_profile' => $partnerProfile ? new PartnerProfileResource($partnerProfile) : null,
+        ];
+    }
+}
