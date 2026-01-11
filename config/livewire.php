693de078e038397b6e723b99246145e82c2d33cd
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-01-11 10:02:53 +0700

    fix: update temporary file upload configuration to use S3 and reduce max file size to 50MB

--- config/livewire.php
+++ config/livewire.php
@@ -64,8 +64,8 @@ return [
     */
 
     'temporary_file_upload' => [
-        'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
-        'rules' => ['required', 'file', 'max:204800'],       // Max 200MB (204800 KB)
+        'disk' => 's3',        // Example: 'local', 's3'              | Default: 'default'
+        'rules' => ['required', 'file', 'max:51200'],       // Max 50MB (51200 KB)
         'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
