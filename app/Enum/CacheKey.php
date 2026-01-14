cf452401d533bb1e9e5c6c041a27a9c12c188217
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-01-15 02:18:21 +0700

    feat: add new cache keys for file product discovery page

--- app/Enum/CacheKey.php
+++ app/Enum/CacheKey.php
@@ -8,4 +8,8 @@ enum CacheKey: string
     case PARTNER_CATEGORIES_TREE = 'partner_categories_tree';
     case PARTNER_CATEGORIES_ALL = 'partner_categories_all';
     case PARTNER_SERVICES = 'partner_services';
+
+    //FileProduct Discover Page
+    case FILE_DISCOVER_CATEGORIES_SIDEBAR = 'discover_categories_sidebar';
+    case FILE_DISCOVER_TAGS_SIDEBAR = 'discover_tags_sidebar';
 }
