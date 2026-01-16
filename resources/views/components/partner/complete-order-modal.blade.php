156641772bb696808b0e2284ee0bcb4e03577fa7
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-01-16 13:30:20 +0700

    feat: update complete order modal to clear photo on close and adjust button action

--- resources/views/components/partner/complete-order-modal.blade.php
+++ resources/views/components/partner/complete-order-modal.blade.php
@@ -19,7 +19,7 @@
         if($refs.photoInput) $refs.photoInput.value = '';
         @this.set('completionPhoto', null);
     }
-}" x-show="showCompleteModal" style="display: none;" @click.self="closeModals()" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
+}" x-show="showCompleteModal" x-on:close-modal.window="closeModals(); clearPhoto()" style="display: none;" @click.self="closeModals()" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
     x-transition:leave="transition ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
     <div class="relative max-w-md mx-4 max-h-[90vh] flex flex-col rounded-xl border border-gray-200 bg-white shadow-2xl dark:bg-gray-800" @click.stop x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0 scale-90" x-transition:enter-end="opacity-100 scale-100" x-transition:leave="transition ease-in duration-200"
         x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-90">
@@ -124,7 +124,7 @@
                 @click="closeModals(); clearPhoto()">
                 {{ __('partner/bill.cancel') }}
             </button>
-            <button class="bg-success-600 hover:bg-success-700 focus:ring-success-500 flex-1 rounded-lg px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-400 disabled:hover:bg-gray-400" type="button" @click="$wire.completeBill(selectedBillId).then(() => { closeModals(); clearPhoto(); })" wire:loading.attr="disabled"
+            <button class="bg-success-600 hover:bg-success-700 focus:ring-success-500 flex-1 rounded-lg px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-400 disabled:hover:bg-gray-400" type="button" @click="$wire.completeBill(selectedBillId)" wire:loading.attr="disabled"
                 wire:target="completeBill" x-bind:disabled="!hasPhoto">
                 <span wire:loading.remove wire:target="completeBill">
                     {{ __('partner/bill.confirm_complete') }}
