02237de434fb80a51a8465cd880955c6376725e6
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-01-21 08:55:24 +0700

    refactor: remove duplicate media conversion methods in PartnerBill model

--- app/Models/PartnerBill.php
+++ app/Models/PartnerBill.php
@@ -153,26 +153,6 @@ class PartnerBill extends Model implements HasMedia
             ->acceptsMimeTypes(['image/jpeg', 'image/png', 'image/jpg', 'image/webp']);
     }
 
-    public function registerMediaConversions(?\Spatie\MediaLibrary\MediaCollections\Models\Media $media = null): void
-    {
-        $this->addMediaConversion('thumb')
-            ->width(400)
-            ->height(400)
-            ->sharpen(10)
-            ->format('webp')
-            ->queued();
-    }
-
-    public function registerMediaConversions(?\Spatie\MediaLibrary\MediaCollections\Models\Media $media = null): void
-    {
-        $this->addMediaConversion('thumb')
-            ->width(400)
-            ->height(400)
-            ->sharpen(10)
-            ->format('webp')
-            ->queued();
-    }
-
     //model boot method
     protected static function boot()
     {
