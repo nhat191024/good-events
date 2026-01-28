8656f007e0443c0ad5cc40cee4c23a0c09d56946
Peredar <64675226+perad4r@users.noreply.github.com>
2026-01-28 14:57:53 +0700

    remove global override

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Inertia\Inertia;
 use Inertia\Response;
-use App\Settings\AppSettings;
 
 class ContactController extends Controller
 {
@@ -13,18 +12,6 @@ class ContactController extends Controller
      */
     public function index(): Response
     {
-        $appSettings = app(AppSettings::class);
-
-        return Inertia::render('contact/Contact', [
-            'app_settings' => [
-                'contact_hotline' => $appSettings->contact_hotline,
-                'contact_email' => $appSettings->contact_email,
-                'social_facebook' => $appSettings->social_facebook,
-                'social_facebook_group' => $appSettings->social_facebook_group,
-                'social_zalo' => $appSettings->social_zalo,
-                'social_youtube' => $appSettings->social_youtube,
-                'social_tiktok' => $appSettings->social_tiktok,
-            ],
-        ]);
+        return Inertia::render('contact/Contact');
     }
 }
