9ad844ce8a897f53077cd69e0c7bc9cb5a147d04
Peredar <64675226+perad4r@users.noreply.github.com>
2026-02-01 07:07:43 +0700

    udpate: add route to tai app route

--- routes/static-pages.php
+++ routes/static-pages.php
@@ -15,3 +15,4 @@ Route::get('/privacy-policy', [StaticPageController::class, 'privacyPolicy'])->n
 Route::get('/shipping-policy-and-payment-methods', [StaticPageController::class, 'shippingPolicy'])->name('static.shipping');
 Route::get('/terms-and-condition', [StaticPageController::class, 'termsAndConditions'])->name('static.terms');
 Route::get('/faq', [StaticPageController::class, 'faq'])->name('static.faq');
+Route::get('/tai-app', [StaticPageController::class, 'downloadApp'])->name('static.download-app');
