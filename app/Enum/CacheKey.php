bce1661aa6e6601becdf8e025788a741772f59a9
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-03-01 17:05:27 +0700

    refactor: reorganize enum cases in CacheKey for better readability

--- app/Enum/CacheKey.php
+++ app/Enum/CacheKey.php
@@ -4,22 +4,26 @@ namespace App\Enum;
 
 enum CacheKey: string
 {
+    case OTP_VERIFICATION = 'otp_verification';
+    case OTP_RESEND_COOLDOWN = 'otp_resend_cooldown';
+
+        //Partner Categories
     case PARTNER_CATEGORIES = 'partner_categories';
     case PARTNER_CATEGORIES_TREE = 'partner_categories_tree';
     case PARTNER_CATEGORIES_ALL = 'partner_categories_all';
     case PARTNER_SERVICES = 'partner_services';
 
-    //FileProduct Discover Page
+        //FileProduct Discover Page
     case FILE_DISCOVER_CATEGORIES_SIDEBAR = 'discover_categories_sidebar';
     case FILE_DISCOVER_TAGS_SIDEBAR = 'discover_tags_sidebar';
 
-    //FileProduct Detail Page
+        //FileProduct Detail Page
     case FILE_CATEGORY_DETAIL = 'file_category_detail';
 
-    //RentProduct Discover Page
+        //RentProduct Discover Page
     case RENT_DISCOVER_CATEGORIES_SIDEBAR = 'rent_discover_categories_sidebar';
     case RENT_DISCOVER_TAGS_SIDEBAR = 'rent_discover_tags_sidebar';
 
-    //RentProduct Detail Page
+        //RentProduct Detail Page
     case RENT_CATEGORY_DETAIL = 'rent_category_detail';
 }
