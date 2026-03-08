4e75c40167015a897a23b45c643f4d605c2c0b3d
Peredar <64675226+perad4r@users.noreply.github.com>
2026-03-08 14:53:18 +0700

    remove: unused routes

--- routes/api/blog.php
+++ routes/api/blog.php
@@ -4,7 +4,5 @@ use App\Http\Controllers\Api\Client\BlogController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('throttle:api')->group(function () {
-    Route::get('/blog/category', [BlogController::class, 'category']);
-    Route::get('/blog/search', [BlogController::class, 'search']);
-    Route::get('/blog/detail', [BlogController::class, 'detail']);
+    Route::get('/blog/home', [BlogController::class, 'home']);
 });
