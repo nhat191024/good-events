135b1c9f0bb7d81d68e9b60e1550e5361467c0e2
Peredar <64675226+perad4r@users.noreply.github.com>
2026-03-08 14:54:19 +0700

    remove: unused routes

--- routes/api/location.php
+++ routes/api/location.php
@@ -5,19 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/locations/{location}/wards', [LocationController::class, 'wards'])
     ->middleware('throttle:api');
-
-Route::middleware(['auth:sanctum', 'throttle:api'])->group(function () {
-    Route::get('/locations', [ResourceController::class, 'index'])
-        ->defaults('resource', 'locations');
-    Route::get('/locations/{id}', [ResourceController::class, 'show'])
-        ->whereNumber('id')
-        ->defaults('resource', 'locations');
-    Route::post('/locations', [ResourceController::class, 'store'])
-        ->defaults('resource', 'locations');
-    Route::post('/locations/{id}', [ResourceController::class, 'update'])
-        ->whereNumber('id')
-        ->defaults('resource', 'locations');
-    Route::post('/locations/{id}/delete', [ResourceController::class, 'destroy'])
-        ->whereNumber('id')
-        ->defaults('resource', 'locations');
-});
+Route::get('/locations', [LocationController::class, 'provinces'])
+    ->middleware('throttle:api');
