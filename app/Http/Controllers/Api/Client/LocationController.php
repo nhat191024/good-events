103de6828f47be462d94ebfd241657349bf44ea0
Peredar <64675226+perad4r@users.noreply.github.com>
2026-03-08 14:43:28 +0700

    update: add endpoint for getting all provinces

--- app/Http/Controllers/Api/Client/LocationController.php
+++ app/Http/Controllers/Api/Client/LocationController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Api\Client;
 
 use App\Http\Controllers\Controller;
 use App\Models\Location;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Cache;
 
 class LocationController extends Controller
 {
@@ -15,15 +15,12 @@ class LocationController extends Controller
      * GET /api/locations/{location}/wards
      *
      * Response: list of ward locations
-     *
-     * @param Location $location
-     * @return JsonResponse
      */
     public function wards(Location $location): JsonResponse
     {
         $cacheKey = "location:{$location->id}:wards";
 
-        $wards = Cache::remember($cacheKey, 60 * 60, function () use ($location) {
+        $wards = Cache::remember($cacheKey, 60 * 9999, function () use ($location) {
             return $location->wards()
                 ->select(['id', 'name', 'code', 'codename', 'short_codename', 'parent_id'])
                 ->orderBy('name')
@@ -31,5 +28,20 @@ class LocationController extends Controller
         });
 
         return response()->json($wards);
+    }
+
+    public function provinces(): JsonResponse
+    {
+        $cacheKey = 'locations:provinces';
+
+        $provinces = Cache::remember($cacheKey, 60 * 9999, function () {
+            return Location::query()
+                ->whereNull('parent_id')
+                ->select(['id', 'name'])
+                ->orderBy('name')
+                ->get();
+        });
+
+        return response()->json($provinces);
     }
 }
