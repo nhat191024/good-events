584eec82055db1464914ee85c55916d2c73bd584
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-03-12 20:12:56 +0700

    feat: dispatch NewPartnerBillCreated event after saving booking info

--- app/Http/Controllers/Client/QuickBookingController.php
+++ app/Http/Controllers/Client/QuickBookingController.php
@@ -3,15 +3,20 @@
 namespace App\Http\Controllers\Client;
 
 use App\Enum\PartnerBillStatus;
-use App\Http\Controllers\Controller;
-use App\Http\Controllers\Services\QuickBookingService;
-use App\Http\Requests\Client\BookingRequest;
 use App\Models\Event;
 use App\Models\Location;
 use App\Models\PartnerBill;
 use App\Models\PartnerCategory;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Services\QuickBookingService;
+use App\Http\Requests\Client\BookingRequest;
+
+use App\Events\NewPartnerBillCreated;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
 use Inertia\Inertia;
 
 /**
@@ -298,7 +303,7 @@ class QuickBookingController extends Controller
             'status' => PartnerBillStatus::PENDING,
         ]);
 
-        $newBill->save();
+        NewPartnerBillCreated::dispatch($newBill);
 
         return redirect()->route('quick-booking.finish', ['bill_code' => $newBill->code]);
     }
