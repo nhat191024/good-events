5e68b7efc33532769e31f8e37bf725009a412db1
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-03-15 19:09:33 +0700

    feat: add history route for retrieving partner bills

--- routes/api/partner/bills.php
+++ routes/api/partner/bills.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\Partner\BillController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/bills/realtime', [BillController::class, 'realtime']);
+Route::get('/bills/history', [BillController::class, 'history']);
 Route::post('/bills/{bill}/accept', [BillController::class, 'accept'])->whereNumber('bill');
 
 Route::get('/bills/{status}', [BillController::class, 'list'])->whereIn('status', ['pending', 'confirmed']);
