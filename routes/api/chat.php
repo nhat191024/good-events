108380be5b2600bbee03eef9daa0f828009ee2c9
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-03-16 22:59:39 +0700

    feat: remove unused loadThreads route from chat API

--- routes/api/chat.php
+++ routes/api/chat.php
@@ -4,6 +4,5 @@ use App\Http\Controllers\Api\Common\ChatController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/chat', [ChatController::class, 'index']);
-Route::get('/chat/threads', [ChatController::class, 'loadThreads']);
 Route::get('/chat/threads/{thread}/messages', [ChatController::class, 'loadMessages'])->whereNumber('thread');
 Route::post('/chat/threads/{thread}/messages', [ChatController::class, 'sendMessage'])->whereNumber('thread');
