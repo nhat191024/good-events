45d28faeed2cdf909b295c9944cbe6cfb8517a13
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-03-18 18:14:48 +0700

    feat: remove partner profile routes and controller

--- routes/api/partner/profile.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\Partner\ProfileController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/profile', [ProfileController::class, 'show']);
-Route::post('/profile', [ProfileController::class, 'update']);
