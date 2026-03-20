81b4253beb15164ad0b5a776942d1214fb865d03
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-03-20 20:46:14 +0700

    feat: enhance ProfileResource to include partner profile details and temporary URLs for images

--- app/Http/Resources/Api/ProfileResource.php
+++ app/Http/Resources/Api/ProfileResource.php
@@ -5,6 +5,8 @@ namespace App\Http\Resources\Api;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+use Illuminate\Support\Facades\Storage;
+
 
 /** @mixin \App\Models\User */
 class ProfileResource extends JsonResource
@@ -34,17 +36,17 @@ class ProfileResource extends JsonResource
             'is_legit' => $partnerProfile->is_legit ?? null,
             'avatar_url' => $this->avatar_url,
             'name' => $this->name,
-            'partner_name' => $partnerProfile->name ?? null,
+            'partner_name' => $partnerProfile->partner_name ?? null,
             'phone' => $this->phone,
             'email' => $this->email,
             'bio' => $this->bio,
             'created_at' => optional($this->created_at)->toIso8601String(),
 
-            'location_id' => $partnerProfile->location_id ?? null,
-            'selfie_image' => $partnerProfile->selfie_image ?? null,
+            'location' => $partnerProfile->location_id ? $partnerProfile->location->name . ' - ' . $partnerProfile->location->province->name ?? null : null,
+            'selfie_image' => $partnerProfile->selfie_image ? Storage::disk('local')->temporaryUrl($partnerProfile->selfie_image, now()->addMinutes(5)) : null,
             'identity_card_number' => $partnerProfile->identity_card_number ?? null,
-            'front_identity_card_image' => $partnerProfile->front_identity_card_image ?? null,
-            'back_identity_card_image' => $partnerProfile->back_identity_card_image ?? null,
+            'front_identity_card_image' => $partnerProfile->front_identity_card_image ? Storage::disk('local')->temporaryUrl($partnerProfile->front_identity_card_image, now()->addMinutes(5)) : null,
+            'back_identity_card_image' => $partnerProfile->back_identity_card_image ? Storage::disk('local')->temporaryUrl($partnerProfile->back_identity_card_image, now()->addMinutes(5)) : null,
         ];
     }
 }
