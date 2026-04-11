01491bc69ee04fb7b839cd2717e6e8409bc29fa8
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-04-11 15:04:13 +0700

    fix: remove unused routes from API

--- routes/api/partner/chat.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\Common\ChatController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/chat', [ChatController::class, 'index']);
-Route::get('/chat/search', [ChatController::class, 'search']);
-Route::get('/chat/threads/{thread}/messages', [ChatController::class, 'loadMessages'])->whereNumber('thread');
-Route::post('/chat/threads/{thread}/messages', [ChatController::class, 'sendMessage'])->whereNumber('thread');
