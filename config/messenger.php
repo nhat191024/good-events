241d068fe673e617238f7096fea1e83ce172df23
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-06-26 21:11:05 +0700

    fix: update message and thread model references to use the correct namespace

--- config/messenger.php
+++ config/messenger.php
@@ -4,11 +4,11 @@ return [
 
     // 'user_model' => App\Models\User::class,
 
-    'message_model' => Cmgmyr\Messenger\Models\Message::class,
+    'message_model' => App\Models\Message::class,
 
     'participant_model' => Cmgmyr\Messenger\Models\Participant::class,
 
-    'thread_model' => Cmgmyr\Messenger\Models\Thread::class,
+    'thread_model' => App\Models\Thread::class,
 
     /**
      * Define custom database table names - without prefixes.
